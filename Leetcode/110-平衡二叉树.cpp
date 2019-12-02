9eaa98860d799537087cc0e8e399fac062c76f2d
Ginakira <ginakira@foxmail.com>
2019-12-02 22:32:26 +0800

    Leetcode / 笔记

---
+++ Leetcode/110-平衡二叉树.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+   public:
+    int countDeep(TreeNode *root) {
+        if (!root) return 0;
+        return max(countDeep(root->left), countDeep(root->right)) + 1;
+    }
+
+    bool isBalanced(TreeNode *root) {
+        if (!root) return true;
+        return isBalanced(root->left) && isBalanced(root->right) &&
+               abs(countDeep(root->left) - countDeep(root->right)) <= 1;
+    }
+};
---
+++ Leetcode/temp.cpp
@@ -0,0 +1,23 @@
+/************************************************************
+    File Name : temp.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Created Time: 2019-12-01 15:28:38
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int number, digit;
+    digit = scanf("%d", &number);
+    printf("%d\n", digit);
+    digit = printf("%d", number);
+    printf("\n%d\n", digit);
+    
+    return 0;
+}
