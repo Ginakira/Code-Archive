5d293bc16132e1b09a0769779c5ce15eef3ec149
Ginakira <ginakira@foxmail.com>
2020-02-16 19:30:16 +0800

    DS

--- HZOJ/DataStructrue/thread_tree.cpp
+++ HZOJ/DataStructrue/thread_tree.cpp
@@ -30,10 +30,10 @@ Node *insert(Node *root, int val) {
     return root;
 }
 
-void build_thread(Node *root) {
+void build_thread_inorder(Node *root) {
     if (!root) return;
     static Node *pre = NULL;
-    build_thread(root->lchild);
+    build_thread_inorder(root->lchild);
     if (!root->lchild) {
         root->lchild = pre;
         root->ltag = THREAD;
@@ -43,7 +43,24 @@ void build_thread(Node *root) {
         pre->rtag = THREAD;
     }
     pre = root;
-    build_thread(root->rchild);
+    build_thread_inorder(root->rchild);
+    return;
+}
+
+void build_thread_postorder(Node *root) {
+    if (!root) return;
+    static Node *pre = NULL;
+    build_thread_postorder(root->lchild);
+    build_thread_postorder(root->rchild);
+    if (!root->lchild) {
+        root->lchild = pre;
+        root->ltag = THREAD;
+    }
+    if (pre && !pre->rchild) {
+        pre->rchild = root;
+        pre->rtag = THREAD;
+    }
+    pre = root;
     return;
 }
 
@@ -52,7 +69,28 @@ Node *most_left(Node *p) {
     return p;
 }
 
-void output(Node *root) {
+void output_postorder(Node *root) {
+    Node *p = root;
+    while (p) {
+        if (p->ltag == NORMAL && p->lchild)
+            p = p->lchild;
+        else if (p->rtag == NORMAL && p->rchild)
+            p = p->rchild;
+        else
+            break;
+    }
+    while (p) {
+        printf("%d ", p->data);
+        if (p->rtag == THREAD) {
+            p = p->rchild;
+        } else {
+            p = most_left(p->rchild);
+        }
+    }
+    return;
+}
+
+void output_inorder(Node *root) {
     Node *p = most_left(root);
     while (p) {
         printf("%d ", p->data);
@@ -72,6 +110,13 @@ void in_order(Node *root) {
     return;
 }
 
+void post_order(Node *root) {
+    if (!root) return;
+    if (root->ltag == NORMAL) post_order(root->lchild);
+    if (root->rtag == NORMAL) post_order(root->rchild);
+    printf("%d ", root->data);
+}
+
 void clear(Node *root) {
     if (!root) return;
     if (root->ltag == NORMAL) clear(root->lchild);
@@ -80,16 +125,21 @@ void clear(Node *root) {
 }
 
 int main() {
-#define max_op 20
+#define max_op 10
     srand(time(0));
     Node *root = NULL;
     for (int i = 0; i < max_op; ++i) {
         int val = rand() % 100;
+        printf("%d ", val);
         root = insert(root, val);
     }
-    build_thread(root);
-    output(root), printf("\n");
-    in_order(root), printf("\n");
+    // build_thread_inorder(root);
+    // printf("Thread-In-Order: "), output_inorder(root), printf("\n");
+    // printf("In-Order:\t "), in_order(root), printf("\n");
+    build_thread_postorder(root);
+    printf("\nPost-Order:\t "), post_order(root), printf("\n");
+    getchar();
+    printf("Thread-Post-Order: "), output_postorder(root), printf("\n");
     clear(root);
     return 0;
 }
