b61abe973ad2bfb5f1838eac36c78a792aee1985
Ginakira <ginakira@foxmail.com>
2020-02-24 19:17:32 +0800

    Leetcode -Search

---
+++ Leetcode/21-合并两个有序链表.cpp
@@ -0,0 +1,45 @@
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// 双指针 12ms 17MB 使用新开链表的方式 边比较边生成新节点
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode(-1), *ans = head;
+        ListNode *p = l1, *q = l2;
+        while (p || q) {
+            if (!q || (p && p->val < q->val)) {
+                ans->next = new ListNode(p->val);
+                p = p->next;
+            } else {
+                ans->next = new ListNode(q->val);
+                q = q->next;
+            }
+            ans = ans->next;
+        }
+        return head->next;
+    }
+};
+
+// 双指针 改变节点之间指向关系 8ms 16.8MB
+class Solution2 {
+   public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(-1);
+        ListNode *pre = head, *p = l1, *q = l2;
+        while (p || q) {
+            if (!q || (p && p->val < q->val)) {
+                pre->next = p;
+                p = p->next;
+            } else {
+                pre->next = q;
+                q = q->next;
+            }
+            pre = pre->next;
+        }
+        return head->next;
+    }
+};
---
+++ Leetcode/4-寻找两个有序数组的中位数.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+// 暴力合并法 40ms 91.8MB O(m + n)
+class Solution {
+   public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int lena = nums1.size(), lenb = nums2.size(), n = lena + lenb;
+        vector<double> ans;
+        for (int p = 0, q = 0, k = 0; k <= n / 2; ++k) {
+            if (q >= lenb || (p < lena && nums1[p] < nums2[q])) {
+                ans.push_back(nums1[p++]);
+            } else {
+                ans.push_back(nums2[q++]);
+            }
+        }
+        return (n & 1) ? ans[n >> 1] : (ans[n / 2 - 1] + ans[n / 2]) / 2.0;
+    }
+};
