5667deca04b96d72786ef721746c2afe7419338d
Ginakira <ginakira@foxmail.com>
2020-02-27 19:24:03 +0800

    LC - stack

--- Leetcode/287-寻找重复数.cpp
+++ Leetcode/287-寻找重复数.cpp
@@ -12,3 +12,23 @@ int findDuplicate(int* nums, int numsSize) {
     }
     return p;
 }
+
+int findDuplicate2(int* nums, int numsSize) {
+    int p = nums[0], q = nums[0];
+    do {
+        p = nums[p];
+        q = nums[nums[q]];
+    } while (p != q);
+    int cnt = 0;
+    do {
+        cnt++;
+        p = nums[p];
+    } while (p != q);
+    p = nums[0], q = nums[0];
+    while (cnt--) q = nums[q];
+    while (p != q) {
+        p = nums[p];
+        q = nums[q];
+    }
+    return p;
+}
