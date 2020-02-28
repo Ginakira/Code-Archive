1322b11e935f174fe89d8ac9b738c325703877e9
Ginakira <ginakira@foxmail.com>
2020-02-28 10:47:27 +0800

    LC - DisjointSet

---
+++ Leetcode/547-朋友圈.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+using namespace std;
+
+// 带路径压缩的裸的并查集 16ms
+class Solution {
+   public:
+    int father[233];
+
+    void init() {
+        for (int i = 0; i < 233; ++i) {
+            father[i] = i;
+        }
+        return;
+    }
+
+    int find(int x) {
+        return father[x] = (father[x] == x ? x : find(father[x]));
+    }
+
+    int merge(int a, int b) {
+        int fa = find(a), fb = find(b);
+        if (fa == fb) return 0;
+        father[fa] = fb;
+        return 1;
+    }
+
+    int findCircleNum(vector<vector<int>>& M) {
+        init();
+        int n = M.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (M[i][j] == 1) merge(i, j);
+            }
+        }
+        int ret = 0;
+        for (int i = 0; i < n; ++i) {
+            if (father[i] == i) ret++;
+        }
+        return ret;
+    }
+};
