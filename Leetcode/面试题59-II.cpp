82aead44b2e14a5c3711d3b88e677b8d00479a2c
Ginakira <ginakira@foxmail.com>
2020-03-07 14:07:15 +0800

    LC59-II - MONOQ

--- Leetcode/面试题59-II.cpp
+++ Leetcode/面试题59-II.cpp
@@ -1,8 +1,9 @@
 // 队列的最大值
 #include <deque>
+#include <queue>
 using namespace std;
 
-// 使用双端队列 144ms
+// 使用双端队列 最大值查询O(N) 144ms
 class MaxQueue {
    private:
     deque<int> dq;
@@ -24,6 +25,37 @@ class MaxQueue {
         if (dq.empty()) return -1;
         int ret = dq.front();
         dq.pop_front();
+        return ret;
+    }
+};
+
+// 队列+双端队列 与单调队列RMQ操作思想相仿 官方说均摊复杂度O(1)
+// 但实际上没有第一种方法快 208ms
+class MaxQueue2 {
+   private:
+    queue<int> q;
+    deque<int> dq;
+
+   public:
+    MaxQueue2() {}
+
+    int max_value() {
+        if (dq.empty()) return -1;
+        return dq.front();
+    }
+
+    void push_back(int value) {
+        q.push(value);
+        while (!dq.empty() && value >= dq.back()) dq.pop_back();
+        dq.push_back(value);
+        return;
+    }
+
+    int pop_front() {
+        if (q.empty()) return -1;
+        int ret = q.front();
+        if (dq.front() == ret) dq.pop_front();
+        q.pop();
         return ret;
     }
 };
