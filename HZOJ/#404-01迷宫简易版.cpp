afd47db6f1530407c1e24e811ebd7259a68cf264
Ginakira <ginakira@foxmail.com>
2020-03-18 19:33:17 +0800

    HZOJ404

--- HZOJ/#404-01迷宫简易版.cpp
+++ HZOJ/#404-01迷宫简易版.cpp
@@ -16,18 +16,18 @@ using namespace std;
 int n, m, sx, sy, ans = 1;
 char grid[3005][3005];
 int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+bool visited[3005][3005];
 
 void dfs(int x, int y) {
     int tx, ty;
-    if (!grid[x][y]) return;
     for (int i = 0; i < 4; ++i) {
         tx = x + dir[i][0], ty = y + dir[i][1];
-        if (grid[tx][ty] != grid[x][y]) {
-            ans++;
+        if (tx < 1 || ty < 1 || tx > n || ty > m) continue;
+        if (!visited[tx][ty] && (grid[tx][ty] != grid[x][y])) {
+            ans++, visited[tx][ty] = 1;
             dfs(tx, ty);
         }
     }
-    grid[tx][ty] = 0;
     return;
 }
 
@@ -37,6 +37,7 @@ int main() {
         cin >> &(grid[i][1]);
     }
     cin >> sx >> sy;
+    visited[sx][sy] = 1;
     dfs(sx, sy);
     cout << ans << endl;
     return 0;
