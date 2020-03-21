84c4a2e5aec0d2f11344ebb119a920ee0cd03ded
Ginakira <ginakira@foxmail.com>
2020-03-21 23:19:06 +0800

    LC - DY

---
+++ Leetcode/LeetCode365.cpp
@@ -0,0 +1,12 @@
+// 每日一题 水壶问题
+#include <numeric>
+using namespace std;
+
+class Solution {
+   public:
+    bool canMeasureWater(int x, int y, int z) {
+        if (x + y < z) return false;
+        if (x == 0 || y == 0) return z == 0 || x + y == z;
+        return !(z % gcd(x, y));
+    }
+};
