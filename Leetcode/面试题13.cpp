6203367b42d2a3193c8d0bcd56be0e65fbb3c735
Ginakira <ginakira@foxmail.com>
2020-04-08 15:11:33 +0800

    LCDY - BFS

--- Leetcode/面试题13.cpp
+++ Leetcode/面试题13.cpp
@@ -36,3 +36,41 @@ class Solution {
 };
 
 // BFS
+class Solution2 {
+   private:
+    int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool mark[105][105];
+    int m, n, ans = 1, k;
+    struct Node {
+        int x, y;
+    };
+    queue<Node> que;
+
+   public:
+    bool is_invalid_position(int x, int y) {
+        if (x < 0 || y < 0 || x > m - 1 || y > n - 1) return false;
+        int sum = 0;
+        while (x) sum += x % 10, x /= 10;
+        while (y) sum += y % 10, y /= 10;
+        return sum <= k;
+    }
+
+    int movingCount(int _m, int _n, int _k) {
+        m = _m, n = _n, k = _k;
+        mark[0][0] = true;
+        que.push({0, 0});
+        Node tmp;
+        while (!que.empty()) {
+            tmp = que.front();
+            que.pop();
+            for (int i = 0; i < 4; ++i) {
+                int tx = tmp.x + dir[i][0], ty = tmp.y + dir[i][1];
+                if (is_invalid_position(tx, ty) && !mark[tx][ty]) {
+                    ans++, mark[tx][ty] = true;
+                    que.push({tx, ty});
+                }
+            }
+        }
+        return ans;
+    }
+};
