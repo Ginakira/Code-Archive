0ed41465e08d4fdf931a3a6a109050c466a22bb1
Ginakira <ginakira@foxmail.com>
2020-04-20 20:04:30 +0800

    UP

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -1,9 +1,9 @@
 /************************************************************
-    File Name : 树状数组-P3374.cpp
+    File Name : temp.cpp
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/04/19 09:09:52
+    Created Time: 2020/04/20 14:48:41
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,48 +11,12 @@
 #include <string>
 #include <vector>
 using namespace std;
-#define MAX_N 500000
-
-inline int lowbit(int x) { return x & (-x); }
-
-int arr[MAX_N + 5];
-int C[MAX_N + 5];
-
-void update(int ind, int val, int n) {
-    while (ind <= n) {
-        C[ind] += val;
-        ind += lowbit(ind);
-    }
-    return;
-}
-
-int query(int ind) {
-    int sum = 0;
-    while (ind) {
-        sum += C[ind];
-        ind -= lowbit(ind);
-    }
-    return sum;
-}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= n; ++i) {
-        cin >> arr[i];
-        update(i, arr[i], n);
-    }
-
-    int a, b, c;
-    while (m--) {
-        cin >> a >> b >> c;
-        if (a == 1) {
-            update(b, c, n);
-        } else {
-            cout << query(c) - query(b - 1) << endl;
-        }
-    }
-
+    int a = 10, b = 32;
+    int const* c = &a;
+    // *c = 66;
+    c = &b;
+    cout << *c << endl;
     return 0;
 }
