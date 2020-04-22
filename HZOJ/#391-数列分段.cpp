b15b2d1100e251c8be2ec5490e12934539fe4f5e
Ginakira <ginakira@foxmail.com>
2020-04-22 18:31:24 +0800

    HZOJ 391

--- HZOJ/#391-数列分段.cpp
+++ HZOJ/#391-数列分段.cpp
@@ -3,7 +3,7 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/04/08 21:06:21
+    Created Time: 2020/04/22 18:14:48
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,42 +11,48 @@
 #include <string>
 #include <vector>
 using namespace std;
-#define MAX_N 100000
 typedef long long LL;
+#define MAX_N 100000
 
-LL n, m, arr[MAX_N + 5], sum_max, num_max;
+LL n, m, num[MAX_N + 5], max_sum, max_num;
 
-LL func(LL tar) {
-    LL cnt = 0, sum = 0;
+int calc(LL mid) {
+    int now = 0, cnt = 0;
     for (int i = 0; i < n; ++i) {
-        if (sum + arr[i] == tar) {
-            cnt++, sum = 0;
-        } else if (sum + arr[i] > tar) {
-            cnt++, sum = arr[i];
-        } else
-            sum += arr[i];
+        if (now + num[i] == mid) {
+            cnt++, now = 0;
+        } else if (now + num[i] > mid) {
+            cnt++, now = num[i];
+        } else {
+            now += num[i];
+        }
     }
-    if (sum > 0) cnt++;
+    if (now > 0) cnt++;
     return cnt;
+}
+
+LL func() {
+    LL l = max_num, r = max_sum;
+    while (l != r) {
+        LL mid = (l + r) >> 1;
+        int cnt = calc(mid);
+        if (cnt > m) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+    return l;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin >> n >> m;
     for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-        sum_max += arr[i];
-        num_max = max(num_max, arr[i]);
+        cin >> num[i];
+        max_num = max(max_num, num[i]);
+        max_sum += num[i];
     }
-    LL l = num_max, r = sum_max;
-    while (l != r) {
-        LL mid = (l + r) >> 1;
-        LL s = func(mid);
-        if (s > m)
-            l = mid + 1;
-        else
-            r = mid;
-    }
-    cout << l << endl;
+    cout << func() << endl;
     return 0;
 }
