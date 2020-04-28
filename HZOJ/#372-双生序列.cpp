5477e074c8f0392d501b718ba58ca871c2ec93b0
Ginakira <ginakira@foxmail.com>
2020-04-29 00:02:12 +0800

    HZOJ372 - QS

---
+++ HZOJ/#372-双生序列.cpp
@@ -0,0 +1,33 @@
+/************************************************************
+    File Name : #372-双生序列.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/04/28 23:53:53
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+#define MAX_N 500000
+int a[MAX_N + 5], b[MAX_N + 5];
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < n; ++i) cin >> b[i];
+    int p = 1, top1 = 0, top2 = 0;
+    while (p < n) {
+        while (top1 != -1 && a[p] <= a[top1]) --top1;
+        while (top2 != -1 && b[p] <= b[top2]) --top2;
+        if (top1 != top2) break;
+        a[++top1] = a[p];
+        b[++top2] = b[p];
+        p++;
+    }
+    cout << p << endl;
+    return 0;
+}
