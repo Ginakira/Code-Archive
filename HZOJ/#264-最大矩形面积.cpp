bd3f9954ce943a27d885858f7e72424480eca70d
Ginakira <ginakira@foxmail.com>
2020-04-28 20:59:38 +0800

    HZ264 MS

--- HZOJ/#264-最大矩形面积.cpp
+++ HZOJ/#264-最大矩形面积.cpp
@@ -2,46 +2,42 @@
     File Name : #264-最大矩形面积.cpp
     Author: Ginakira
     Mail: ginakira@outlook.com
-    Created Time: 2020/02/16 20:11:03
+    Github: https://github.com/Ginakira
+    Created Time: 2020/04/28 20:46:26
 ************************************************************/
 #include <algorithm>
 #include <cmath>
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
-#define MAXN 100000
+#define MAX_N 100000
 typedef long long LL;
 
-int h[MAXN + 5];
-LL l[MAXN + 5], r[MAXN + 5];
-int s[MAXN + 5], top = -1;
+LL a[MAX_N + 5];
+int s[MAX_N + 5], top = -1;
+int l[MAX_N + 5], r[MAX_N + 5];
 
 int main() {
     int n;
     cin >> n;
-    h[0] = h[n + 1] = -1;
+    for (int i = 1; i <= n; ++i) cin >> a[i];
+    a[0] = a[n + 1] = -1;
+    s[top = 0] = 0;
     for (int i = 1; i <= n; ++i) {
-        cin >> h[i];
-    }
-    s[++top] = 0;
-    for (int i = 1; i <= n; ++i) {
-        while (h[i] <= h[s[top]]) top--;
+        while (a[s[top]] >= a[i]) --top;
         l[i] = s[top];
         s[++top] = i;
     }
-    top = -1;
-    s[++top] = n + 1;
+    s[top = 0] = n + 1;
     for (int i = n; i >= 1; --i) {
-        while (h[i] <= h[s[top]]) top--;
+        while (a[s[top]] >= a[i]) --top;
         r[i] = s[top];
         s[++top] = i;
     }
-    long long ans = 0;
+    LL ans = 0;
     for (int i = 1; i <= n; ++i) {
-        ans = max(ans, (r[i] - l[i] - 1) * h[i]);
+        ans = max(ans, a[i] * (r[i] - l[i] - 1));
     }
     cout << ans << endl;
     return 0;
