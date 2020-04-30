ec3a2128af400f8cd7e5eab98e2fbe283fb2e439
Ginakira <ginakira@foxmail.com>
2020-04-30 18:36:22 +0800

    HZ333 - ST

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -3,7 +3,7 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/04/28 17:13:59
+    Created Time: 2020/04/29 23:12:06
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,27 +11,64 @@
 #include <string>
 #include <vector>
 using namespace std;
+#define MAX_N 1000
 
-int f[1005][15][15];
+struct BigInt : public vector<int> {
+    BigInt(int x = 0) {
+        push_back(x);
+        normal();
+    }
+
+    BigInt operator*(int x) {
+        BigInt ret(*this);
+        for (int i = 0; i < ret.size(); ++i) {
+            ret[i] *= x;
+        }
+        ret.normal();
+        return ret;
+    }
+
+    BigInt operator+(const BigInt &a) {
+        BigInt ret(*this);
+        for (int i = 0; i < a.size(); ++i) {
+            if (i < ret.size())
+                ret[i] += a[i];
+            else
+                ret.push_back(a[i]);
+        }
+        ret.normal();
+        return ret;
+    }
+
+    void normal() {
+        for (int i = 0; i < size(); ++i) {
+            if (at(i) < 10) continue;
+            if (i + 1 == size()) push_back(0);
+            at(i + 1) += at(i) / 10;
+            at(i) %= 10;
+        }
+        return;
+    }
+};
+
+BigInt f[MAX_N + 5];
+
+ostream &operator<<(ostream &out, const BigInt &a) {
+    for (int i = a.size() - 1; i >= 0; --i) {
+        out << a[i];
+    }
+    return out;
+}
 
 int main() {
     int n, k;
     cin >> n >> k;
-
-    for (int i = 0; i < k; ++i) {
-        for (int j = 0; j < k; ++j) {
-            f[1][i][j] = (i == j);
-        }
+    f[1] = k;
+    f[2] = k * (k - 1);
+    f[3] = k * (k - 1) * (k - 2);
+    for (int i = 4; i <= n; ++i) {
+        f[i] = f[i - 1] * (k - 2) + f[i - 2] * (k - 1);
     }
-
-    for (int t = 2; t <= n; ++t) {
-        for (int i = 0; i < k; ++i) {
-            for (int j = 0; j < k; ++j) {
-                for (int l = 0; l < k; ++l) {
-                }
-            }
-        }
-    }
-
+    cout << f[n] << endl;
     return 0;
 }
