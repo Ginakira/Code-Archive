2c2bceac9ef0392b3212d628f77cf148d2a93c94
Ginakira <ginakira@foxmail.com>
2020-05-02 11:41:20 +0800

    DPPt

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -3,7 +3,7 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/04/29 23:12:06
+    Created Time: 2020/05/02 11:14:50
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,64 +11,25 @@
 #include <string>
 #include <vector>
 using namespace std;
-#define MAX_N 1000
-
-struct BigInt : public vector<int> {
-    BigInt(int x = 0) {
-        push_back(x);
-        normal();
-    }
-
-    BigInt operator*(int x) {
-        BigInt ret(*this);
-        for (int i = 0; i < ret.size(); ++i) {
-            ret[i] *= x;
-        }
-        ret.normal();
-        return ret;
-    }
-
-    BigInt operator+(const BigInt &a) {
-        BigInt ret(*this);
-        for (int i = 0; i < a.size(); ++i) {
-            if (i < ret.size())
-                ret[i] += a[i];
-            else
-                ret.push_back(a[i]);
-        }
-        ret.normal();
-        return ret;
-    }
-
-    void normal() {
-        for (int i = 0; i < size(); ++i) {
-            if (at(i) < 10) continue;
-            if (i + 1 == size()) push_back(0);
-            at(i + 1) += at(i) / 10;
-            at(i) %= 10;
-        }
-        return;
-    }
-};
-
-BigInt f[MAX_N + 5];
-
-ostream &operator<<(ostream &out, const BigInt &a) {
-    for (int i = a.size() - 1; i >= 0; --i) {
-        out << a[i];
-    }
-    return out;
-}
+#define MAX_M 20
+#define MAX_N 10000
+int arr[MAX_M + 5];
+int dp[MAX_M + 5][MAX_N + 5];
 
 int main() {
-    int n, k;
-    cin >> n >> k;
-    f[1] = k;
-    f[2] = k * (k - 1);
-    f[3] = k * (k - 1) * (k - 2);
-    for (int i = 4; i <= n; ++i) {
-        f[i] = f[i - 1] * (k - 2) + f[i - 2] * (k - 1);
+    int n, m;
+    cin >> m >> n;
+    for (int i = 1; i <= m; ++i) {
+        cin >> arr[i];
+        dp[i][0] = 1;
     }
-    cout << f[n] << endl;
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            dp[i][j] = dp[i - 1][j];
+            if (j >= arr[i]) dp[i][j] += dp[i][j - arr[i]];
+            dp[i][j] %= 9973;
+        }
+    }
+    cout << dp[m][n] << endl;
     return 0;
 }
