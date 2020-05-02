2f5a9758c63310b1ba95cef5f7702d4b66b10a9a
Ginakira <ginakira@foxmail.com>
2020-05-02 11:58:05 +0800

    DPPT

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -3,7 +3,7 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/05/02 11:14:50
+    Created Time: 2020/05/02 11:51:14
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,25 +11,18 @@
 #include <string>
 #include <vector>
 using namespace std;
-#define MAX_M 20
-#define MAX_N 10000
-int arr[MAX_M + 5];
-int dp[MAX_M + 5][MAX_N + 5];
+#define MAX_N 32767
+int dp[MAX_N + 5];
 
 int main() {
-    int n, m;
-    cin >> m >> n;
-    for (int i = 1; i <= m; ++i) {
-        cin >> arr[i];
-        dp[i][0] = 1;
+    int n;
+    cin >> n;
+    dp[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = (dp[i - 1] + 2 - 1) % i + 1;
     }
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            dp[i][j] = dp[i - 1][j];
-            if (j >= arr[i]) dp[i][j] += dp[i][j - arr[i]];
-            dp[i][j] %= 9973;
-        }
-    }
-    cout << dp[m][n] << endl;
+    int x = n;
+    while (dp[x] - x) x = dp[x];
+    cout << n + x << endl;
     return 0;
 }
