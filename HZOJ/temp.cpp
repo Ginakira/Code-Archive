5c6924924802048f1afd47695b06fc340abcdac4
Ginakira <ginakira@foxmail.com>
2020-05-04 00:33:09 +0800

    DPPC

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -3,7 +3,7 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/05/03 23:48:04
+    Created Time: 2020/05/04 00:20:56
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,35 +11,22 @@
 #include <string>
 #include <vector>
 using namespace std;
-#define MAX_N 1000000
-#define INF 0x3f3f3f3f
-
-int f[MAX_N + 5];
-int val[MAX_N + 5];
-int len[MAX_N + 5], ans = 0;
-
-int bs(int *arr, int l, int r, int x) {
-    if (l == r) return l;
-    int mid = (l + r) >> 1;
-    if (arr[mid] < x) return bs(arr, mid + 1, r, x);
-    return bs(arr, l, mid, x);
-}
+#define MAX_N 1000
+int dp[MAX_N + 5][MAX_N + 5];
 
 int main() {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        cin >> val[i];
-        f[i] = 1;
-        len[i] = INF;
+    string s1, s2;
+    cin >> s1 >> s2;
+    int len1 = s1.size(), len2 = s2.size();
+    for (int i = 1; i <= len1; ++i) {
+        for (int j = 1; j <= len2; ++j) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
     }
-
-    len[++ans] = val[1];
-    for (int i = 2; i <= n; ++i) {
-        f[i] = bs(len, 1, ans + 1, val[i]);
-        len[f[i]] = val[i];
-        ans = max(ans, f[i]);
-    }
-    cout << ans << endl;
+    cout << dp[len1][len2] << endl;
     return 0;
 }
