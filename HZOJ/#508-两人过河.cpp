fc41196f813dcab3928a478bd49665e935abd027
Ginakira <ginakira@foxmail.com>
2020-05-13 19:18:59 +0800

    HZ508 - Greedy

---
+++ HZOJ/#508-两人过河.cpp
@@ -0,0 +1,38 @@
+/************************************************************
+    File Name : #508-两人过河.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/05/13 19:14:34
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+#define MAX_N 1000
+int n, num[MAX_N + 5], ans;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> num[i];
+    }
+    sort(num, num + n);
+    for (int i = n - 1; i >= 0; i -= 2) {
+        if (i == 0) {
+            ans += num[0];
+            break;
+        } else if (i == 1) {
+            ans += num[1];
+            break;
+        } else if (i == 2) {
+            ans += num[0] + num[1] + num[2];
+            break;
+        }
+        ans += min(num[1] + num[0] + num[i] + num[1],
+                   num[i] + num[0] + num[i - 1] + num[0]);
+    }
+    cout << ans << endl;
+    return 0;
+}
