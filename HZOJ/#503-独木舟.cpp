965eb7406990207c9fe1bf2c86694fb2cba42bb3
Ginakira <ginakira@foxmail.com>
2020-05-13 18:07:46 +0800

    HZ503 Greedy

--- HZOJ/#503-独木舟.cpp
+++ HZOJ/#503-独木舟.cpp
@@ -2,33 +2,31 @@
     File Name : #503-独木舟.cpp
     Author: Ginakira
     Mail: ginakira@outlook.com
-    Created Time: 2020/01/28 14:49:47
+    Github: https://github.com/Ginakira
+    Created Time: 2020/05/13 18:02:10
 ************************************************************/
 #include <algorithm>
 #include <cmath>
-#include <cstdio>
-#include <iomanip>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-#define MAXN 30000
-
-int weight[MAXN + 5];
-bool is_shipped[MAXN + 5];
+#define MAX_N 30000
+int a[MAX_N + 5];
+bool is_shipped[MAX_N + 5];
 
 int main() {
-    int w, n;
+    int w, n, ans = 0;
     cin >> w >> n;
     for (int i = 0; i < n; ++i) {
-        cin >> weight[i];
+        cin >> a[i];
     }
-    sort(weight, weight + n);
-    int ans = 0;
+    sort(a, a + n);
     for (int i = 0; i < n; ++i) {
         if (is_shipped[i]) continue;
         ans++, is_shipped[i] = true;
         for (int j = n - 1; j > i; --j) {
-            if (!is_shipped[j] && weight[j] + weight[i] <= w) {
+            if (!is_shipped[j] && w - a[i] >= a[j]) {
                 is_shipped[j] = true;
                 break;
             }
