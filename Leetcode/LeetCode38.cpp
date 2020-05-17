2b5738baddbf5139897e81cfc1264ad98f6295d5
Ginakira <ginakira@foxmail.com>
2020-05-17 14:27:52 +0800

    LC38

--- Leetcode/LeetCode38.cpp
+++ Leetcode/LeetCode38.cpp
@@ -20,3 +20,24 @@ class Solution {
         return ans;
     }
 };
+
+class Solution2 {
+   public:
+    string countAndSay(int n) {
+        if (n == 1) return string("1");
+        string pre_str = countAndSay(n - 1);
+        char str[10000];
+        int ret = 0, num = pre_str[0], cnt = 1;
+        for (int i = 1; pre_str[i]; ++i) {
+            if (pre_str[i] == num)
+                cnt++;
+            else {
+                ret += sprintf(str + ret, "%d%c", cnt, num);
+                num = pre_str[i];
+                cnt = 1;
+            }
+        }
+        ret += sprintf(str + ret, "%d%c", cnt, num);
+        return str;
+    }
+};
