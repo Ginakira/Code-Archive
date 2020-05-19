77f9e7a2b8770cc88543aa6b9bfa183f073cd07c
Ginakira <ginakira@foxmail.com>
2020-05-19 20:10:58 +0800

    LC8

--- Leetcode/LeetCode8.cpp
+++ Leetcode/LeetCode8.cpp
@@ -37,3 +37,25 @@ class Solution {
         return (int)ans * neg_flag;
     }
 };
+
+// Haizei
+class Solution {
+   public:
+    int myAtoi(string str) {
+        int flag = 1, num = 0, i = 0, pre_max = INT_MAX / 10, d = INT_MAX % 10;
+        while (str[i] == ' ') ++i;
+        if (str[i] == '-')
+            flag = -1, ++i;
+        else if (str[i] == '+')
+            flag = 1, ++i;
+        while (str[i]) {
+            if (str[i] < '0' || str[i] > '9') break;
+            if (num > pre_max || (num == pre_max && (str[i] - '0') > d)) {
+                return flag == 1 ? INT_MAX : INT_MIN;
+            }
+            num = num * 10 + (str[i] - '0');
+            ++i;
+        }
+        return flag * num;
+    }
+};
