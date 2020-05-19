1b3ba8dfcf4edb779852605ab36a0ab48f06f569
Ginakira <ginakira@foxmail.com>
2020-05-19 20:49:39 +0800

    LC36 BITMASK

---
+++ Leetcode/Leetcode36.cpp
@@ -0,0 +1,25 @@
+// 有效的数独
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int x[9], y[9], z[9];  // 掩码 0可放 1不可放
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; ++i) x[i] = y[i] = z[i] = 0;
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') continue;
+                int num = board[i][j] - '0';
+                if (x[i] & (1 << num)) return false;
+                if (y[j] & (1 << num)) return false;
+                if (z[i / 3 * 3 + j / 3] & (1 << num)) return false;
+                x[i] |= (1 << num);
+                y[j] |= (1 << num);
+                z[i / 3 * 3 + j / 3] |= (1 << num);
+            }
+        }
+        return true;
+    }
+};
