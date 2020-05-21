efcd5d61de70fa74891503bec64210085ef3bebc
Ginakira <ginakira@foxmail.com>
2020-05-21 18:21:26 +0800

    LC165 OPTIMIZED

--- Leetcode/Leetcode165.cpp
+++ Leetcode/Leetcode165.cpp
@@ -25,3 +25,27 @@ class Solution {
         return 0;
     }
 };
+
+// 优化
+class Solution2 {
+   public:
+    int getNext(string &str, int i, int &val) {  // 返回下一个版本号的位置
+        val = 0;
+        while (str[i] && str[i] <= '9' && str[i] >= '0') {
+            val = val * 10 + str[i] - '0';
+            i++;
+        }
+        if (str[i]) ++i;
+        return i;
+    }
+
+    int compareVersion(string version1, string version2) {
+        int i = 0, j = 0, val1 = 0, val2 = 0;
+        while (version1[i] || version2[j]) {
+            i = getNext(version1, i, val1);
+            j = getNext(version2, j, val2);
+            if (val1 - val2) return val1 > val2 ? 1 : -1;
+        }
+        return 0;
+    }
+};
