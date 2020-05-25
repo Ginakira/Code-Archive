c358a98d5db386b389808ebc46a2ab691f3e387a
Ginakira <ginakira@foxmail.com>
2020-05-25 20:21:33 +0800

    HZ246

--- HZOJ/#246-七夕祭.cpp
+++ HZOJ/#246-七夕祭.cpp
@@ -3,7 +3,7 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/05/25 19:05:18
+    Created Time: 2020/05/25 19:52:34
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,10 +11,46 @@
 #include <string>
 #include <vector>
 using namespace std;
+typedef long long LL;
+#define MAX_N 100000
+
+LL x[MAX_N + 5], y[MAX_N + 5];
+LL sum[MAX_N + 5];
+LL n, m, c;
+
+LL solve(LL *arr, LL n, LL avg) {
+    for (int i = 1; i <= n; ++i) {
+        sum[i] = sum[i - 1] + arr[i] - avg;
+    }
+    sort(sum + 1, sum + n + 1);
+    LL val = sum[(n + 1) >> 1], ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        ans += abs(sum[i] - val);
+    }
+    return ans;
+}
 
 int main() {
-    
-
-
+    cin >> n >> m >> c;
+    for (int i = 0; i < c; ++i) {
+        int a, b;
+        cin >> a >> b;
+        x[a]++, y[b]++;
+    }
+    LL ans = 0;
+    if (c % n == 0 && c % m == 0) {
+        cout << "both ";
+        ans = solve(x, n, c / n) + solve(y, m, c / m);
+    } else if (c % n == 0) {
+        cout << "row ";
+        ans = solve(x, n, c / n);
+    } else if (c % m == 0) {
+        cout << "column ";
+        ans = solve(y, m, c / m);
+    } else {
+        cout << "impossible\n";
+        return 0;
+    }
+    cout << ans << endl;
     return 0;
 }
