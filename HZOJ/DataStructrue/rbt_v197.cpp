575a6422ae469c84e82daf695de49e23b0571db9
Ginakira <ginakira@foxmail.com>
2020-05-26 23:52:20 +0800

    rbtprc

--- HZOJ/DataStructrue/rbt_v197.cpp
+++ HZOJ/DataStructrue/rbt_v197.cpp
@@ -3,25 +3,26 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/05/18 23:32:13
+    Created Time: 2020/05/26 23:24:42
 ************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
-
-enum colors { RED, BLACK, DOUBLE_BLACK };
+#define RED 0
+#define BLACK 1
+#define DOUBLE_BLACK 2
 
 typedef struct Node {
-    int key, color;  // 0 RED 1 BLACK 2 DOUBLE_BLACK
+    int key, color;
     struct Node *lchild, *rchild;
 } Node;
 
-Node __NIL, *const NIL = &__NIL;
+Node _NIL, *const NIL = &_NIL;
 
 __attribute__((constructor)) void init_NIL() {
-    NIL->lchild = NIL->rchild = NIL;
     NIL->key = 0;
-    NIL->color = BLACK;
+    NIL->lchild = NIL->rchild = NIL;
+    NIL->color = RED;
     return;
 }
 
@@ -29,11 +30,10 @@ Node *getNewNode(int key) {
     Node *p = (Node *)malloc(sizeof(Node));
     p->key = key;
     p->lchild = p->rchild = NIL;
-    p->color = RED;
     return p;
 }
 
-bool has_red_child(Node *root) {
+int hasRedChild(Node *root) {
     return root->lchild->color == RED || root->rchild->color == RED;
 }
 
@@ -52,26 +52,26 @@ Node *right_rotate(Node *root) {
 }
 
 Node *insert_maintain(Node *root) {
-    if (!has_red_child(root)) return root;
-    if (root->lchild->color == RED && root->rchild->color == RED) {
-        if (!has_red_child(root->lchild) && !has_red_child(root->rchild))
-            return root;
-        goto insert_end;
+    if (!hasRedChild(root)) return root;
+    if (root->lchild->color == RED && root->rchild->color == RED &&
+        (hasRedChild(root->lchild) || hasRedChild(root->rchild))) {
+        root->color = RED;
+        root->lchild->color = root->rchild->color = BLACK;
+        return root;
     }
     if (root->lchild->color == RED) {  // L
-        if (!has_red_child(root->lchild)) return root;
+        if (!hasRedChild(root->lchild)) return root;
         if (root->lchild->rchild->color == RED) {  // LR
             root->lchild = left_rotate(root->lchild);
         }
         root = right_rotate(root);
-    } else {  // R
-        if (!has_red_child(root->rchild)) return root;
-        if (root->rchild->lchild->color == RED) {  // RL
+    } else {
+        if (!hasRedChild(root->rchild)) return root;
+        if (root->rchild->lchild->color == RED) {
             root->rchild = right_rotate(root->rchild);
         }
         root = left_rotate(root);
     }
-insert_end:
     root->color = RED;
     root->lchild->color = root->rchild->color = BLACK;
     return root;
@@ -80,11 +80,10 @@ insert_end:
 Node *__insert(Node *root, int key) {
     if (root == NIL) return getNewNode(key);
     if (root->key == key) return root;
-    if (root->key > key) {
+    if (root->key > key)
         root->lchild = __insert(root->lchild, key);
-    } else {
+    else
         root->rchild = __insert(root->rchild, key);
-    }
     return insert_maintain(root);
 }
 
@@ -94,35 +93,10 @@ Node *insert(Node *root, int key) {
     return root;
 }
 
-void clearNode(Node *root) {
+void clear(Node *root) {
     if (root == NIL) return;
-    clearNode(root->lchild);
-    clearNode(root->rchild);
+    clear(root->lchild);
+    clear(root->rchild);
     free(root);
     return;
-}
-
-void output(Node *root) {
-    if (root == NIL) return;
-    printf("%d [%d, %d] %s\n", root->key, root->lchild->key, root->rchild->key,
-           root->color ? "BLACK" : "RED");
-    output(root->lchild);
-    output(root->rchild);
-    return;
-}
-
-int main() {
-    Node *root = NIL;
-    int op, val;
-    while (~scanf("%d%d", &op, &val)) {
-        switch (op) {
-            case 1: {
-                root = insert(root, val);
-            } break;
-            case 2: {
-            } break;
-        }
-        output(root);
-    }
-    return 0;
 }
