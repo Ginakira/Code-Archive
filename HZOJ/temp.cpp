a88eb898aafafae43a566866ace6ff4fa1b83e28
Ginakira <ginakira@foxmail.com>
2020-05-28 00:01:54 +0800

    rbt pc

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -3,32 +3,104 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/05/27 22:26:18
+    Created Time: 2020/05/27 23:33:03
 ************************************************************/
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
-#define MAX_N 500000
-int a[MAX_N + 5], b[MAX_N + 5];
-int qa[MAX_N + 5], qb[MAX_N + 5];
-int top1 = -1, top2 = -1;
 
-int main() {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; ++i) cin >> a[i];
-    for (int i = 1; i <= n; ++i) cin >> b[i];
-    int p = 1;
-    while (p <= n) {
-        while (top1 != -1 && a[qa[top1]] >= a[p]) top1--;
-        while (top2 != -1 && b[qb[top2]] >= b[p]) top2--;
-        if (top1 != top2) break;
-        qa[++top1] = p, qb[++top2] = p;
-        p++;
-    }
-    cout << p - 1 << endl;
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RED 0
+#define BLACK 1
+#define DBLACK 2
+
+typedef struct Node {
+    int key, color;
+    struct Node *lchild, *rchild;
+} Node;
+
+Node __NIL, *const NIL = &__NIL;
+
+__attribute__((constructor)) void init_NIL() {
+    NIL->key = 0;
+    NIL->lchild = NIL->rchild = NIL;
+    NIL->color = BLACK;
+    return;
 }
+
+Node *getNewNode(int key) {
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->key = key;
+    p->lchild = p->rchild = NIL;
+    p->color = RED;
+    return p;
+}
+
+int has_red_child(Node *root) {
+    return root->lchild->color == RED || root->rchild->color == RED;
+}
+
+Node *left_rotate(Node *root) {
+    Node *temp = root->rchild;
+    root->rchild = temp->lchild;
+    temp->lchild = root;
+    return temp;
+}
+
+Node *right_rotate(Node *root) {
+    Node *temp = root->lchild;
+    root->lchild = temp->rchild;
+    temp->rchild = root;
+    return temp;
+}
+
+Node *insert_maintain(Node *root) {
+    if (!has_red_child(root)) return root;
+    if (root->lchild->color == RED && root->rchild->color == RED &&
+        (has_red_child(root->lchild) || has_red_child(root->rchild))) {
+        root->color = RED;
+        root->lchild->color = root->rchild->color = BLACK;
+        return root;
+    }
+    if (root->lchild->color == RED) {  // L
+        if (!has_red_child(root->lchild)) return root;
+        if (root->lchild->rchild->color == RED) {  // LR
+            root->lchild = left_rotate(root->lchild);
+        }
+        root = right_rotate(root);
+    } else {  // R
+        if (!has_red_child(root->rchild)) return root;
+        if (root->rchild->lchild->color == RED) {  // RL
+            root->rchild = right_rotate(root->rchild);
+        }
+        root = left_rotate(root);
+    }
+    root->color = RED;
+    root->lchild->color = root->rchild->color = BLACK;
+    return root;
+}
+
+Node *__insert(Node *root, int key) {
+    if (root == NIL) return getNewNode(key);
+    if (root->key == key) return root;
+    if (root->key > key)
+        root->lchild = __insert(root->lchild, key);
+    else
+        root->rchild = __insert(root->rchild, key);
+    return insert_maintain(root);
+}
+
+Node *insert(Node *root, int key) {
+    root = __insert(root, key);
+    root->color = BLACK;
+    return root;
+}
+
+void clear(Node *root) {
+    if (root == NIL) return;
+    clear(root->lchild);
+    clear(root->rchild);
+    free(root);
+    return;
+}
+
+int main() { return 0; }
