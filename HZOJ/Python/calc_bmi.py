e4216118b2cd980667b1aa1dfa7168e06be48e12
Ginakira <ginakira@foxmail.com>
2020-07-09 19:19:03 +0800

    python practice

---
+++ HZOJ/Python/calc_bmi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+height = 1.77  # m
+weight = 65  # kg
+
+bmi = weight / (height * height)
+print(bmi)
---
+++ HZOJ/Python/shop_change.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+money_all = 123.6 + 23.7 + 53.4 + 34.5
+
+print("总金额：" + str(money_all))
+
+money_pay = int(money_all)
+
+print("实收：" + str(money_pay))
