60a1a29cb3d0c53c8e211dbc007297ff3ad625be
Ginakira <ginakira@foxmail.com>
2020-07-12 11:14:57 +0800

    v3

--- HZOJ/DataStructrue/quick_sort/quick_sort_v3.cc
+++ HZOJ/DataStructrue/quick_sort/quick_sort_v3.cc
@@ -6,18 +6,20 @@
     Created Time: 2020/07/10 10:52:35
 ************************************************************/
 
+#include <algorithm>
+
 #include "quick_sort.h"
 
 // 区间左闭右闭
 // 无监督partition
 void quick_sort_v3(int *arr, int l, int r) {
     while (l < r) {
-        int x = l, y = r, z = arr[l], temp;
+        int x = l, y = r, z = arr[l];
         do {
             while (arr[x] < z) x++;
             while (arr[y] > z) y--;
             if (x <= y) {
-                temp = arr[x], arr[x] = arr[y], arr[y] = temp;
+                std::swap(arr[x], arr[y]);
                 x++, y--;
             }
         } while (x <= y);
