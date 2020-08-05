43afda426ddff5cbf9fe2f87e135feea0f5e91ef
Ginakira <ginakira@foxmail.com>
2020-08-06 00:18:57 +0800

    prac

--- HZOJ/CPP/temp.cpp
+++ HZOJ/CPP/temp.cpp
@@ -3,31 +3,26 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/08/05 16:05:58
+    Created Time: 2020/08/05 22:15:46
 ************************************************************/
 #include <algorithm>
 #include <cmath>
-#include <cstring>
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 
 class A {
    public:
-    A() { this->arr = new int[100]; }
-    A(const A &obj) : x(obj.x), y(obj.y) {
-        this->arr = new int[100];
-        memcpy(this->arr, obj.arr, sizeof(int) * 100);
-    }
-    int *arr;
-    int x, y;
+    A() { cout << "constructor" << endl; }
+    A(const A &obj) { cout << "copy constructor" << endl; }
 };
 
+A func() {
+    A temp;
+    return temp;
+}
 int main() {
-    A a;
-    A b = a;
-    a.arr[0] = 123;
-    b.arr[0] = 456;
-    cout << a.arr[0] << ' ' << b.arr[0] << endl;
+    A a(func()); 
     return 0;
 }
