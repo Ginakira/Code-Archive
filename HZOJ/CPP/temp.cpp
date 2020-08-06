5fdd65d8d3bb5c50900efea8e960813e41737098
Ginakira <ginakira@foxmail.com>
2020-08-06 21:42:42 +0800

    simple cout

--- HZOJ/CPP/temp.cpp
+++ HZOJ/CPP/temp.cpp
@@ -3,7 +3,7 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/08/06 15:34:04
+    Created Time: 2020/08/06 21:12:43
 ************************************************************/
 #include <algorithm>
 #include <cmath>
@@ -11,11 +11,42 @@
 #include <string>
 #include <vector>
 
-#include "template.h"
-using namespace std;
+namespace haizei {
+class ostream {
+   public:
+    ostream &operator<<(int x) {
+        printf("%d", x);
+        return *this;
+    }
+
+    ostream &operator<<(const std::string &str) {
+        printf("%s", str.c_str());
+        return *this;
+    }
+
+    ostream &operator<<(const char &ch) {
+        printf("%c", ch);
+        return *this;
+    }
+
+    ostream &operator<<(const double &a) {
+        char e_str[20];
+        sprintf(e_str, "%-6g", a);
+        for (int i = 0; e_str[i]; ++i) {
+            if (e_str[i] - ' ') continue;
+            e_str[i] = 0;
+            break;
+        }
+        printf("%s", e_str);
+        return *this;
+    }
+};
+char endl = '\n';
+ostream cout;
+}  // namespace haizei
 
 int main() {
-    cout << add(2, 3) << endl;
-    cout << func() << endl;
+    haizei::cout << "Hello world" << haizei::endl;
+    haizei::cout << 123 << haizei::endl;
     return 0;
 }
