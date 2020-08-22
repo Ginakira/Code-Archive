91e40946bb67672a545587c873178cf8d61b72bd
Ginakira <ginakira@foxmail.com>
2020-08-22 16:07:59 +0800

    responsibility link

---
+++ HZOJ/CPP/41.responsibility_link.cpp
@@ -0,0 +1,27 @@
+/************************************************************
+    File Name : 41.responsibility_link.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/08/22 10:49:01
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    // int score;
+    // if (score < 60) {
+    //     father.fight();
+    // } else if (score < 70) {
+    //     father.nothing();
+    //     mother.dudu();
+    // } else if (score < 80) {
+    //     mother.prize();
+    // }
+    
+    return 0;
+}
