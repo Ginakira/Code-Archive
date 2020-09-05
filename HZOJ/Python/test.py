16f8705148b7e655c60dc1758b476220e0ffc433
Ginakira <ginakira@foxmail.com>
2020-09-05 11:46:51 +0800

    test

---
+++ HZOJ/Python/test.py
@@ -0,0 +1,17 @@
+params = {"plan_id": 1, "show_all": 2, "days": 4}
+
+valid = []
+
+result = 0
+
+valid.append(params["show_all"])
+valid.append(params["days"])
+valid.append(params["show_all"] | params["plan_id"])
+valid.append(params["days"] | params["plan_id"])
+
+print(valid)
+
+result = params["plan_id"] | params["show_all"]
+print(result)
+
+print(result in valid)
