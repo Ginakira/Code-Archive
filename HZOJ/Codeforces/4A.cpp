4d7a1d496c994a91c80249ac0ec429ace207399a
Ginakira <ginakira@foxmail.com>
2020-09-07 23:02:42 +0800

    cf4a

---
+++ HZOJ/Codeforces/4A.cpp
@@ -0,0 +1,20 @@
+/************************************************************
+    File Name : 4A.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/09/07 22:50:45
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    cout << ((w > 2 && w % 2 == 0) ? "YES" : "NO") << endl;
+    return 0;
+}
