7d2472ac6cce9bfbaa16b98eb2febe5be0db3b12
Ginakira <ginakira@foxmail.com>
2020-09-08 09:02:23 +0800

    143A

---
+++ HZOJ/Codeforces/143A.cpp
@@ -0,0 +1,25 @@
+/************************************************************
+    File Name : 143A.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/09/07 23:14:32
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b + c >= 2) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
