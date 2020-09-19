fe23458bb521057b59c825443a67bb8068b28c7c
Ginakira <ginakira@foxmail.com>
2020-09-19 15:05:20 +0800

    cf 50a

---
+++ HZOJ/Codeforces/50A.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Author: Your name
+ * @Date:   2020-09-18 15:05:07
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2020-09-18 15:05:07
+ */
+/************************************************************
+    File Name : 50A.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/09/15 18:14:51
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() { return 0; }
