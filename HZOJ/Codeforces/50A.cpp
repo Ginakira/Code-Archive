766b31f8154d3458d02f3b5370e7e568b81ec7b4
Ginakira <ginakira@foxmail.com>
2020-09-20 14:17:54 +0800

    practice

--- HZOJ/Codeforces/50A.cpp
+++ HZOJ/Codeforces/50A.cpp
@@ -1,21 +1,22 @@
-/*
- * @Author: Your name
- * @Date:   2020-09-18 15:05:07
- * @Last Modified by:   Your name
- * @Last Modified time: 2020-09-18 15:05:07
- */
 /************************************************************
     File Name : 50A.cpp
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/09/15 18:14:51
+    Created Time: 2020/09/21 12:36:24
 ************************************************************/
 #include <algorithm>
+#include <cerrno>
 #include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-int main() { return 0; }
+int main() {
+    char str[] = {"Hello world!"};
+    sprintf(str, "(%s)", str);
+    if (errno != 0) perror("ERRNO:");
+    printf("%s\n", str);
+    return 0;
+}
