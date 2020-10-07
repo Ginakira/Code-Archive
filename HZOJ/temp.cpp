9c4db801412ba8b4487bd2968b18d26c68e2c38e
Ginakira <ginakira@foxmail.com>
2020-10-07 19:44:07 +0800

    test

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -3,24 +3,17 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/10/05 16:43:08
+    Created Time: 2020/10/09 19:11:11
 ************************************************************/
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 #include <iostream>
-#include <string>
 #include <vector>
 using namespace std;
 
 int main() {
-    char str[] =
-        "I love you so much, Ms.Ginger.\nMy love is like a flame burning in my "
-        "heart, making it difficult for me to think about anyone but "
-        "you.\nPlease accept my love.\n\nWith love,\nMr.Garlic";
-    char *tok = NULL;
-    tok = strtok(str, "\t\n\r\n \"\',.");
-    do {
-        printf("%s\n", tok);
-    } while ((tok = strtok(NULL, "\t\n\r\n \"\',.")) != NULL);
+    char str[16] = "hello world!";
+    printf("%c\n", *(str + strlen(str) - 4));
     return 0;
 }
