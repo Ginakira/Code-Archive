1ea487ff4635b1626c2321635ccf8c39001bc965
Ginakira <ginakira@foxmail.com>
2020-10-12 15:53:52 +0800

    test

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -1,30 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/************************************************************
+    File Name : temp.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/10/12 12:33:59
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
 int main() {
-    int n;
-    scanf("%d\n", &n);
-
-    int i = 0;
-    int j;
-    int *len = (int *)calloc(n, sizeof(int));
-    char *name = (char *)calloc(n, sizeof(char));
-    while (scanf("%[^\n]%*c", name) != EOF) {
-        len[i] = strlen(&name[i]);
-        i++;
-        if (i == n) {
-            break;
-        }
-    }
-
-    for (j = 0; j < n; j++) {
-        if (len[j] > len[j + 1]) {
-            strcpy(&name[j + 1], &name[j]);
-        }
-        if (j == n - 2) {
-            printf("%c", name[n - 1]);
-            break;
-        }
-    }
+    int num1 = 0x616263;
+    int num2 = 0x61626364;
+    printf("%s\n", (char *)(&num2 + 1));
+    printf("%p\n", &num1);
+    printf("%p\n", &num2);
     return 0;
 }
