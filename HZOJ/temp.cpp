4031c842ae76eda86e7e82c1ceb581392e258d49
Ginakira <ginakira@foxmail.com>
2020-10-14 11:38:25 +0800

    test scanf

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -3,20 +3,24 @@
     Author: Ginakira
     Mail: ginakira@outlook.com
     Github: https://github.com/Ginakira
-    Created Time: 2020/10/12 12:33:59
+    Created Time: 2020/10/13 22:19:36
 ************************************************************/
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
 int main() {
-    int num1 = 0x616263;
-    int num2 = 0x61626364;
-    printf("%s\n", (char *)(&num2 + 1));
-    // printf("%p\n", &num1);
-    // printf("%p\n", &num2);
+    char str[50];
+    while (scanf("%[^\n]\n", str) != EOF) {
+        printf("==============\n");
+        for (int i = 0; i < 10; ++i) {
+            printf("%d ", (int)str[i]);
+        }
+        printf("\n==============\n");
+    }
     return 0;
 }
