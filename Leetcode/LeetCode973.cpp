00c1a7bb28e1fc2b7a8ad1d190d1a718f1bc13f6
Ginakira <ginakira@foxmail.com>
2020-11-10 11:18:27 +0800

    LC973 SL2

--- Leetcode/LeetCode973.cpp
+++ Leetcode/LeetCode973.cpp
@@ -1,17 +1,48 @@
 // 973. 最接近原点的 K 个点
 #include <algorithm>
+#include <queue>
 #include <vector>
 using namespace std;
 
 // Solution 1 STL
 class Solution {
-public:
+   public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        nth_element(points.begin(), points.begin() + K, points.end(), [](auto &&a, auto &&b) {
-            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
-        });
+        nth_element(points.begin(), points.begin() + K, points.end(),
+                    [](auto&& a, auto&& b) {
+                        return a[0] * a[0] + a[1] * a[1] <
+                               b[0] * b[0] + b[1] * b[1];
+                    });
         points.resize(K);
         return points;
     }
 };
 
+// Solution 2 Priority Queue
+class Solution2 {
+   public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<pair<int, int>> q;
+        for (int i = 0; i < K; ++i) {
+            q.emplace(points[i][0] * points[i][0] + points[i][1] * points[i][1],
+                      i);
+        }
+
+        int n = points.size();
+        for (int i = K; i < n; ++i) {
+            int dist =
+                points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            if (q.top().first > dist) {
+                q.pop();
+                q.emplace(dist, i);
+            }
+        }
+
+        vector<vector<int>> ans;
+        while (!q.empty()) {
+            ans.emplace_back(points[q.top().second]);
+            q.pop();
+        }
+        return ans;
+    }
+};
