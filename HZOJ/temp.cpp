fad7ee4ea8d4f23ea545b340bc703b9b1f11d339
Ginakira <ginakira@foxmail.com>
2020-11-10 10:37:02 +0800

    temp

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -1,26 +1,55 @@
-/************************************************************
-    File Name : temp.cpp
-    Author: Ginakira
-    Mail: ginakira@outlook.com
-    Github: https://github.com/Ginakira
-    Created Time: 2020/10/28 21:55:08
-************************************************************/
-#include <algorithm>
-#include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-int main() {
-    int num = 3, cnt = 0;
-    while (num) {
-        if (num & 1) {
-            cnt++;
-        }
-        num >>= 1;
-        cout << "\033[32mCount one digit\033[0m" << endl;
+double f(double x, double y) { return y - (2 * x) / y; }
+//欧拉公式
+vector<double> Euler(double x0, double y0, double h, int N) {
+    vector<double> Y(N, 0);
+    double x = x0;
+    Y[0] = y0;
+    for (int n = 1; n < N; n++) {
+        Y[n] = Y[n - 1] + h * f(x, Y[n - 1]);
+        x += h;
     }
-    cout << cnt << endl;
+    return Y;
+}
+//改进欧拉公式
+vector<double> ImprovedEuler(double x0, double y0, double h, int N) {
+    vector<double> Y(N, 0);
+    Y[0] = y0;
+    double x = x0;
+    double p = 0;
+    double c = 0;
+    for (int n = 1; n < N; n++) {
+        p = Y[n - 1] + h * f(x, Y[n - 1]);
+        x += h;
+        c = Y[n - 1] + h * f(x, p);
+        Y[n] = (p + c) / 2;
+    }
+    return Y;
+}
+
+int main() {
+    cout << "请输入步长 h 和要计算的函数值的个数 N ： " << endl;
+    double h;
+    int N;
+    cin >> h >> N;
+    cout << "请输入初始函数点（x0，y0）：" << endl;
+    double x0;
+    double y0;
+    cin >> x0 >> y0;
+    vector<double> Y1 = Euler(x0, y0, h, N + 1);
+    cout << "欧拉公式的计算结果为：  " << endl;
+    for (int i = 0; i < N + 1; i++) {
+        cout << x0 + i * h << "     " << Y1[i] << endl;
+    }
+    vector<double> Y2 = ImprovedEuler(x0, y0, h, N + 1);
+
+    cout << "改进欧拉公式的计算结果为：  " << endl;
+    for (int i = 0; i < N + 1; i++) {
+        cout << x0 + i * h << "     " << Y2[i] << endl;
+    }
     return 0;
 }
