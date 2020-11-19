af975739fe086979432498e139ea6f7cbfd9910d
Ginakira <ginakira@foxmail.com>
2020-11-19 16:40:34 +0800

    LC36

--- Leetcode/Leetcode36.cpp
+++ Leetcode/Leetcode36.cpp
@@ -23,3 +23,28 @@ class Solution {
         return true;
     }
 };
+
+class Solution2 {
+   public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool row[9][10]{{false}};
+        bool col[9][10]{{false}};
+        bool box[9][10]{{false}};
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') continue;
+                int current = board[i][j] - '0';
+                if (row[i][current]) return false;
+                if (col[j][current]) return false;
+                if (box[j / 3 + (i / 3) * 3][current]) return false;
+
+                row[i][current] = true;
+                col[j][current] = true;
+                box[j / 3 + (i / 3 * 3)][current] = true;
+            }
+        }
+
+        return true;
+    }
+};
