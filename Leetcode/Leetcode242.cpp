949f286b8e4bae0408fc3e01728680a4019990da
Ginakira <ginakira@foxmail.com>
2020-11-20 17:08:58 +0800

    LC242

--- Leetcode/Leetcode242.cpp
+++ Leetcode/Leetcode242.cpp
@@ -7,10 +7,10 @@ class Solution {
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
         int code[256] = {0};
-        for (char c : s) code[c] += 1;
-        for (char c : t) {
-            if (code[c] == 0) return false;
-            code[c] -= 1;
+        for (char &c : s) code[c]++;
+        for (char &c : t) {
+            if (!code[c]) return false;
+            code[c]--;
         }
         return true;
     }
