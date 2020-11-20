20bae5daf94566b6902bce532864ab8a76ce5d1a
Ginakira <ginakira@foxmail.com>
2020-11-20 16:12:25 +0800

    LC344

--- Leetcode/Leetcode344.cpp
+++ Leetcode/Leetcode344.cpp
@@ -5,8 +5,9 @@ using namespace std;
 class Solution {
    public:
     void reverseString(vector<char>& s) {
-        for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
-            swap(s[i], s[j]);
+        int n = s.size();
+        for (int i = 0; i < n / 2; ++i) {
+            swap(s[i], s[n - i - 1]);
         }
         return;
     }
