f88eb20d19e0587438b63a2768e27006a9d24893
Ginakira <ginakira@foxmail.com>
2020-12-03 17:51:40 +0800

    LC204

--- Leetcode/LeetCode204.cpp
+++ Leetcode/LeetCode204.cpp
@@ -1,4 +1,6 @@
 // LeetCode 204 计数质数
+#include <vector>
+using namespace std;
 
 // Solution1: 线性筛法 104ms 27.7MB
 class Solution {
@@ -15,5 +17,22 @@ class Solution {
             }
         }
         return prime[0];
+    }
+};
+
+// C++写法优化内存占用 220ms 9.5MB
+class Solution2 {
+   public:
+    int countPrimes(int n) {
+        vector<int> primes;
+        vector<bool> is_prime(n, 1);
+        for (int i = 2; i < n; ++i) {
+            if (is_prime[i]) primes.push_back(i);
+            for (int j = 0; j < primes.size() && i * primes[j] < n; ++j) {
+                is_prime[primes[j] * i] = false;
+                if (!(i % primes[j])) break;
+            }
+        }
+        return primes.size();
     }
 };
