b14606708ac1ed6da4e04e00bf34a4864ab3897a
Ginakira <ginakira@foxmail.com>
2020-12-09 22:45:20 +0800

    LC62S2

--- Leetcode/LeetCode62.cpp
+++ Leetcode/LeetCode62.cpp
@@ -23,3 +23,19 @@ class Solution {
         return dp[n - 1][m - 1];
     }
 };
+
+// Solution2: 组合公式
+// 从左上角到右下角的过程中，我们需要移动 m+n-2 次，其中有 m-1
+// 次向下移动，n-1 次向右移动。因此路径的总数，就等于从 m+n-2
+// 次移动中选择 m-1 次向下移动的方案数，即组合数C(m-1)(m+n-2)
+
+class Solution2 {
+   public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return ans;
+    }
+};
