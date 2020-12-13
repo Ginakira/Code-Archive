4c987fbaab818e90ae20ef96a7bb7e38a2f06669
Ginakira <ginakira@foxmail.com>
2020-12-13 22:42:17 +0800

    LC5625

---
+++ Leetcode/LeetCode5625.cpp
@@ -0,0 +1,26 @@
+// LeetCode 5625 字节跳动周赛 比赛中的配对次数
+
+// 普通做法
+class Solution1 {
+   public:
+    int numberOfMatches(int n) {
+        int cnt = 0;
+        while (n > 1) {
+            if (n % 2) {
+                cnt += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            } else {
+                cnt += n / 2;
+                n /= 2;
+            }
+        }
+        return cnt;
+    }
+};
+
+// 看破红尘
+// 实际上每次配对都会淘汰一支队伍，所以一定会进行n - 1次配对，最后剩下1支队伍
+class Solution2 {
+   public:
+    int numberOfMatches(int n) { return n - 1; }
+};
