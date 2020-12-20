9d0d556de7d406c36cc68f12e301cb8ac0be2bdd
Ginakira <ginakira@foxmail.com>
2020-12-20 22:25:27 +0800

    temp

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -1,41 +1,20 @@
+/************************************************************
+    File Name : temp.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2020/12/20 19:54:32
+************************************************************/
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int n, m, sx, sy;
-char mmap[105][105];
-int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-int func(int x, int y) {
-    for (int i = 0; i < 4; i++) {
-        int xx = x + dir[i][0];
-        int yy = y + dir[i][1];
-        if (mmap[xx][yy] == 'T') {
-            return 1;
-        }
-        if (mmap[xx][yy] == '.') {
-            mmap[xx][yy] = '#';
-            if (func(xx, yy) == 1) {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
 int main() {
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> mmap[i][j];
-            if (mmap[i][j] == 'S') {
-                sx = i, sy = j;
-            }
-        }
-    }
-    if (func(sx, sy) == 1) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
-    }
+    int a = 233, *p = &a;
+    delete p;
+    cout << a << endl;
     return 0;
 }
