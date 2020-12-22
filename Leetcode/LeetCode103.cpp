b55a1ce6c2484ebb807d956a19c87a9014f75bb6
Ginakira <ginakira@foxmail.com>
2020-12-22 21:40:15 +0800

    LC 103

---
+++ Leetcode/LeetCode103.cpp
@@ -0,0 +1,32 @@
+// LeetCode 103 二叉树的锯齿形层序遍历
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+   public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root) {
+        vector<vector<int>> nodes;
+        pushNodes(nodes, root, 0);
+        for (int i = 0; i < nodes.size(); ++i) {
+            if (i % 2) reverse(nodes[i].begin(), nodes[i].end());
+        }
+        return nodes;
+    }
+
+    void pushNodes(vector<vector<int>> &nodes, TreeNode *root, int depth) {
+        if (!root) return;
+        if (nodes.size() <= depth) nodes.emplace_back();
+        nodes[depth].emplace_back(root->val);
+        pushNodes(nodes, root->left, depth + 1);
+        pushNodes(nodes, root->right, depth + 1);
+        return;
+    }
+};
