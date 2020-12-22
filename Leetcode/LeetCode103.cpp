ac31677170c804839284779bf2ab99d892324259
Ginakira <ginakira@foxmail.com>
2020-12-22 21:44:15 +0800

    LC103

--- Leetcode/LeetCode103.cpp
+++ Leetcode/LeetCode103.cpp
@@ -10,22 +10,24 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// Solution1: 先递归按层存储所有二叉树节点 然后翻转下标奇数层
+// Solution1: 递归按层存储所有二叉树节点 奇数层倒序插入（头插）
+// 树根为第0层  8ms
 class Solution {
    public:
     vector<vector<int>> zigzagLevelOrder(TreeNode *root) {
         vector<vector<int>> nodes;
         pushNodes(nodes, root, 0);
-        for (int i = 0; i < nodes.size(); ++i) {
-            if (i % 2) reverse(nodes[i].begin(), nodes[i].end());
-        }
         return nodes;
     }
 
     void pushNodes(vector<vector<int>> &nodes, TreeNode *root, int depth) {
         if (!root) return;
         if (nodes.size() <= depth) nodes.emplace_back();
-        nodes[depth].emplace_back(root->val);
+        if (depth % 2) {
+            nodes[depth].insert(nodes[depth].begin(), root->val);
+        } else {
+            nodes[depth].emplace_back(root->val);
+        }
         pushNodes(nodes, root->left, depth + 1);
         pushNodes(nodes, root->right, depth + 1);
         return;
