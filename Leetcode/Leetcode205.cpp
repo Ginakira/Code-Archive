d68800b6d2ed0b4f9eb32dafb802f2fd00a4bf32
Ginakira <ginakira@foxmail.com>
2020-12-27 16:02:44 +0800

    LC 205

--- Leetcode/Leetcode205.cpp
+++ Leetcode/Leetcode205.cpp
@@ -1,8 +1,9 @@
 // 同构字符串
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
-// 一一对应的绑定关系
+// 一一对应的绑定关系 4ms
 class Solution {
    public:
     bool isIsomorphic(string s, string t) {
@@ -17,6 +18,26 @@ class Solution {
             }
             int val = s_code[s[i]];
             if (val - t[i]) return false;
+        }
+        return true;
+    }
+};
+
+// unordered_map 24ms
+class Solution2 {
+   public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> s_mp, t_mp;
+        if (s.size() != t.size()) return false;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (s_mp.find(s[i]) == s_mp.end() &&
+                t_mp.find(t[i]) == t_mp.end()) {
+                s_mp[s[i]] = t[i];
+                t_mp[t[i]] = s[i];
+            } else if (s_mp[s[i]] != t[i] || t_mp[t[i]] != s[i]) {
+                return false;
+            }
         }
         return true;
     }
