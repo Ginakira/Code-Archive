7864a2bc59d92472c0cfcd2a2dda91b4c78a9afb
Ginakira <ginakira@foxmail.com>
2021-01-01 23:40:21 +0800

    LC 28

--- Leetcode/Leetcode28.cpp
+++ Leetcode/Leetcode28.cpp
@@ -23,3 +23,20 @@ class Solution {
         return -1;
     }
 };
+
+// 朴素匹配
+class Solution2 {
+   public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+        int pa = 0, pb = 0;
+        while (pa < haystack.size() && pb < needle.size()) {
+            if (haystack[pa] != needle[pb]) {
+                pa -= pb - 1, pb = 0;
+                continue;
+            }
+            ++pa, ++pb;
+        }
+        return pb == needle.size() ? pa - pb : -1;
+    }
+};
