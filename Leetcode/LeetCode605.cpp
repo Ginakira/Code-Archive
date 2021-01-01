486dea45dfea7e9681f2729ebe17b38be4330b2c
Ginakira <ginakira@foxmail.com>
2021-01-01 13:59:16 +0800

    LC605

---
+++ Leetcode/LeetCode605.cpp
@@ -0,0 +1,22 @@
+// LeetCode 605 种花问题
+#include <vector>
+using namespace std;
+
+// Solution: 贪心
+// 首位各添加一个0，简化边界判断
+// 只要碰到连续的三个零，即当前位、前一位、后一位均为零，即可在当前位种一朵花
+class Solution {
+   public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int cnt = 0;
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
+        for (int i = 1; i < flowerbed.size() - 1; ++i) {
+            if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 &&
+                flowerbed[i + 1] == 0) {
+                cnt++, flowerbed[i] = 1;
+            }
+        }
+        return n <= cnt;
+    }
+};
