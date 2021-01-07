8117b7ce907637f3c1cc14a20cbd06f762b8d717
Ginakira <ginakira@foxmail.com>
2021-01-07 21:35:04 +0800

    LC 228

---
+++ Leetcode/LeetCode228.cpp
@@ -0,0 +1,23 @@
+// LeetCode 228 汇总区间
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ret;
+        for (int i = 0, n = nums.size(), pre = 0; i < n; ++i) {
+            if (i == n - 1 || nums[i] + 1 != nums[i + 1]) {
+                if (i - pre > 0) {
+                    ret.push_back(to_string(nums[pre]) + "->" +
+                                  to_string(nums[i]));
+                } else {
+                    ret.push_back(to_string(nums[i]));
+                }
+                pre = i + 1;
+            }
+        }
+        return ret;
+    }
+};
