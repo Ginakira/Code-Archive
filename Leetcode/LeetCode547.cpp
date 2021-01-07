a528f5ee0bcdf24aaab325d430743fb601f1ea66
Ginakira <ginakira@foxmail.com>
2021-01-07 11:29:01 +0800

    LC547

--- Leetcode/LeetCode547.cpp
+++ Leetcode/LeetCode547.cpp
@@ -5,38 +5,45 @@ using namespace std;
 // 带路径压缩的裸的并查集 16ms
 class Solution {
    public:
-    int father[233];
+    struct UnionSet {
+        int* father;
+        int n;
 
-    void init() {
-        for (int i = 0; i < 233; ++i) {
-            father[i] = i;
+        UnionSet(int n) : n(n) {
+            father = new int[n];
+            for (int i = 0; i < n; ++i) father[i] = i;
         }
-        return;
-    }
 
-    int find(int x) {
-        return father[x] = (father[x] == x ? x : find(father[x]));
-    }
+        int find(int x) { return x == father[x] ? x : find(father[x]); }
 
-    int merge(int a, int b) {
-        int fa = find(a), fb = find(b);
-        if (fa == fb) return 0;
-        father[fa] = fb;
-        return 1;
-    }
+        void merge(int a, int b) {
+            int ra = find(a), rb = find(b);
+            if (ra == rb) return;
+            father[rb] = ra;
+            return;
+        }
 
-    int findCircleNum(vector<vector<int>>& M) {
-        init();
-        int n = M.size();
+        int getRootCount() {
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                if (father[i] == i) ++cnt;
+            }
+            return cnt;
+        }
+
+        ~UnionSet() { delete[] father; }
+    };
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        UnionSet u(n);
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (M[i][j] == 1) merge(i, j);
+                if (isConnected[i][j] == 1) {
+                    u.merge(i, j);
+                }
             }
         }
-        int ret = 0;
-        for (int i = 0; i < n; ++i) {
-            if (father[i] == i) ret++;
-        }
-        return ret;
+        return u.getRootCount();
     }
 };
