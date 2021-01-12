401ad6d89b22a6deacd989c4a0fc4ec97ed520ca
Ginakira <ginakira@foxmail.com>
2021-01-12 15:37:58 +0800

    LC210BFS

--- Leetcode/LeetCode210.cpp
+++ Leetcode/LeetCode210.cpp
@@ -1,5 +1,6 @@
 // LeetCode 210 课程表II
 #include <algorithm>
+#include <queue>
 #include <vector>
 using namespace std;
 
@@ -43,5 +44,43 @@ class Solution {
         visited[u] = SEARCHED;
         result.push_back(u);
         return;
+    }
+};
+
+// 基于BFS的拓扑排序，统计入度，每次删除入度0的边，加入答案
+class Solution2 {
+   private:
+    vector<vector<int>> edges;
+    vector<int> indeg;
+
+   public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        indeg.resize(numCourses);
+        for (const auto& info : prerequisites) {
+            edges[info[1]].push_back(info[0]);
+            ++indeg[info[0]];
+        }
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (indeg[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> ret;
+        while (!q.empty()) {
+            int u = q.front();
+            for (auto& v : edges[u]) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    q.push(v);
+                }
+            }
+            ret.push_back(u);
+            q.pop();
+        }
+        if (ret.size() != numCourses) return {};
+        return ret;
     }
 };
