1feea14f27b62ac0e0fec9f009e1f2624a6749e9
Ginakira <ginakira@foxmail.com>
2021-01-26 17:38:40 +0800

    LC415 M

--- Leetcode/LeetCode415.cpp
+++ Leetcode/LeetCode415.cpp
@@ -1,5 +1,6 @@
 // LeetCode 415 字符串相加
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
@@ -17,5 +18,51 @@ class Solution {
         }
         if (num1[0] == '0' && num1.size() > 1) num1.erase(0, 1);
         return num1;
+    }
+};
+
+// BigInt类
+class BigInt : vector<int> {
+   public:
+    BigInt(string s) {
+        int n = s.size();
+        for (int i = n - 1; i >= 0; --i) {
+            emplace_back(s[i] - '0');
+        }
+    }
+
+    BigInt operator+(BigInt &b) {
+        BigInt result = *this;
+        int n = b.size(), len = result.size() > n ? result.size() : n;
+        result.resize(len);
+        for (int i = 0; i < len; ++i) {
+            if (i < n) {
+                result[i] += b[i];
+            }
+            if (result[i] >= 10) {
+                if (i + 1 == result.size()) {
+                    result.emplace_back(0);
+                }
+                result[i + 1] += result[i] / 10;
+                result[i] %= 10;
+            }
+        }
+        return result;
+    }
+
+    string stringify() {
+        string s;
+        for (auto it = rbegin(); it != rend(); ++it) {
+            s.push_back(*it + '0');
+        }
+        return s;
+    }
+};
+
+class Solution2 {
+   public:
+    string addStrings(string num1, string num2) {
+        BigInt a(num1), b(num2);
+        return (a + b).stringify();
     }
 };
