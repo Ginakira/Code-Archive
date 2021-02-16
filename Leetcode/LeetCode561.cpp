79f9f070a549261860944e53ef8430d0bf715ce6
Ginakira <ginakira@foxmail.com>
2021-02-16 11:43:34 +0800

    LC561

---
+++ Leetcode/LeetCode561.cpp
@@ -0,0 +1,17 @@
+// LeetCodee 561 数组拆分
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// 贪心 排序
+class Solution {
+   public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i += 2) {
+            ans += nums[i];
+        }
+        return ans;
+    }
+};
