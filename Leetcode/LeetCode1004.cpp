3ac9d37717f54b4d3eaf6b2a629d9b4e0f26897c
Ginakira <ginakira@foxmail.com>
2021-02-19 19:39:29 +0800

    LC1004

--- Leetcode/LeetCode1004.cpp
+++ Leetcode/LeetCode1004.cpp
@@ -23,3 +23,21 @@ class Solution {
         return right - left;
     }
 };
+
+// 更简短的版本 count代表当前窗口内需要翻转多少个0
+class Solution2 {
+   public:
+    int longestOnes(vector<int>& A, int K) {
+        int n = A.size();
+        int left = 0, right = 0, count = 0;
+        while (right < n) {
+            count += !A[right];
+            if (count > K) {
+                count -= !A[left];
+                ++left;
+            }
+            ++right;
+        }
+        return right - left;
+    }
+};
