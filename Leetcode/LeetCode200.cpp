19c9ecae6b03a3f7d479d73b488f3ce15903f34e
Ginakira <ginakira@foxmail.com>
2021-02-28 19:09:30 +0800

    LC 200

--- Leetcode/LeetCode200.cpp
+++ Leetcode/LeetCode200.cpp
@@ -56,3 +56,43 @@ class Solution {
         return ans;
     }
 };
+
+// DFS
+class Solution2 {
+   private:
+    int dir[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    int rows, cols;
+    vector<vector<bool>> visited;
+
+   public:
+    int numIslands(vector<vector<char>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
+        visited.resize(rows, vector<bool>(cols, false));
+        int ans = 0;
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    ++ans;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return ans;
+    }
+
+    void dfs(int i, int j, vector<vector<char>>& grid) {
+        if (visited[i][j]) return;
+        visited[i][j] = true;
+        for (int k = 0; k < 4; ++k) {
+            int nx = i + dir[k][0], ny = j + dir[k][1];
+            if (nx < 0 || nx >= rows || ny < 0 || ny >= cols ||
+                grid[nx][ny] == '0') {
+                continue;
+            }
+            dfs(nx, ny, grid);
+        }
+        return;
+    }
+};
