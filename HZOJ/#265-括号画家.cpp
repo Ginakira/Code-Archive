976dd441ae1b89291d0acd7e6f7a147c8fe11d5b
Ginakira <ginakira@foxmail.com>
2021-03-18 11:03:40 +0800

    HZ265

---
+++ HZOJ/#265-括号画家.cpp
@@ -0,0 +1,24 @@
+/************************************************************
+    File Name : #265-括号画家.cpp
+    Author: Ginakira
+    Mail: ginakira@outlook.com
+    Github: https://github.com/Ginakira
+    Created Time: 2021/03/17 16:57:33
+************************************************************/
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+unordered_map<char, char> match = {{'[', ']'}, {'(', ')'}, {'{', '}'}};
+
+inline bool is_right(char c) { return c == ']' || c == ')' || c == '}'; }
+
+int main() {
+    
+    return 0;
+}
