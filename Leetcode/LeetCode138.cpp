064259af8a96a81acf7e27a5961da89a13f6fc03
Ginakira <ginakira@foxmail.com>
2021-03-28 21:18:23 +0800

    LC138

--- Leetcode/LeetCode138.cpp
+++ Leetcode/LeetCode138.cpp
@@ -15,6 +15,7 @@ class Node {
     }
 };
 
+// 哈希表
 class Solution {
    public:
     Node *copyRandomList(Node *head) {
@@ -36,5 +37,38 @@ class Solution {
             p = p->next;
         }
         return old_to_new[head];
+    }
+};
+
+// 直接在原节点后面复制，复制完成后改写指向关系
+class Solution2 {
+   public:
+    Node *copyRandomList(Node *head) {
+        if (head == nullptr) return nullptr;
+        Node *p = head;
+        while (p != nullptr) {
+            Node *new_node = new Node(p->val);
+            new_node->next = p->next;
+            p->next = new_node;
+            p = new_node->next;
+        }
+        p = head;
+        while (p != nullptr) {
+            if (p->random != nullptr) {
+                p->next->random = p->random->next;
+            }
+            p = p->next->next;
+        }
+        p = head;
+        Node *ret = p->next;
+        while (p != nullptr) {
+            Node *node = p->next, *q = node->next;
+            if (q != nullptr) {
+                node->next = q->next;
+            }
+            p->next = q;
+            p = q;
+        }
+        return ret;
     }
 };
