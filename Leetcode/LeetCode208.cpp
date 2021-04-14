cb606dacfd38670e6f042d662db10b75944845dd
Ginakira <ginakira@foxmail.com>
2021-04-14 16:15:01 +0800

    LC 208

--- Leetcode/LeetCode208.cpp
+++ Leetcode/LeetCode208.cpp
@@ -5,71 +5,58 @@
 using std::string;
 using std::unordered_map;
 
-class TrieNode {
-   public:
-    TrieNode(char _c) : c(_c) {}
-    friend class Trie;
-
-   private:
-    char c;
-    bool isWord = false;
-    unordered_map<char, TrieNode *> child;
-};
-
 class Trie {
+   private:
+    class TrieNode {
+       public:
+        TrieNode(char c) : c(c) {}
+        char c;
+        bool is_word = false;
+        unordered_map<char, TrieNode *> children;
+    };
+
+    TrieNode *root;
+
+    TrieNode *find(string &word) {
+        TrieNode *current = root;
+        for (char c : word) {
+            if (current->children.find(c) == current->children.end()) {
+                return nullptr;
+            }
+            current = current->children[c];
+        }
+        return current;
+    }
+
    public:
     /** Initialize your data structure here. */
-    Trie() : root(new TrieNode('/')) {}
+    Trie() { root = new TrieNode('='); }
 
     /** Inserts a word into the trie. */
     void insert(string word) {
-        TrieNode *p = root;
-        for (const auto &c : word) {
-            if (p->child.find(c) != p->child.end()) {
-                p = p->child[c];
-            } else {
-                TrieNode *node = new TrieNode(c);
-                p->child[c] = node;
-                p = node;
+        TrieNode *current = root;
+        for (char c : word) {
+            if (current->children.find(c) == current->children.end()) {
+                current->children[c] = new TrieNode(c);
             }
+            current = current->children[c];
         }
-        p->isWord = true;
+        current->is_word = true;
         return;
     }
 
     /** Returns if the word is in the trie. */
     bool search(string word) {
         TrieNode *result = find(word);
-        if (result == nullptr || result->isWord == false) {
-            return false;
-        }
-        return true;
+        return result != nullptr ? result->is_word : false;
     }
 
     /** Returns if there is any word in the trie that starts with the given
      * prefix. */
     bool startsWith(string prefix) {
         TrieNode *result = find(prefix);
-        if (result == nullptr) {
-            return false;
-        }
-        return true;
+        return result != nullptr;
     }
-
-    TrieNode *find(string word) {
-        TrieNode *p = root;
-        for (const auto &c : word) {
-            if (p->child.find(c) != p->child.end()) {
-                p = p->child[c];
-            } else {
-                return nullptr;
-            }
-        }
-        return p;
-    }
-
-   private:
-    TrieNode *root;
 };
 
 /**
