026e60d7e446931da4dd510ab47b88b71ef51938
Ginakira <ginakira@foxmail.com>
2021-04-15 21:57:07 +0800

    LC 213

--- Leetcode/LeetCode213.cpp
+++ Leetcode/LeetCode213.cpp
@@ -4,22 +4,20 @@ using namespace std;
 
 class Solution {
    public:
+    int helper(vector<int>& nums, int start, int end) {
+        int ans = nums[start], pre = nums[start], ppre = 0;
+        for (int i = start + 1; i <= end; ++i) {
+            ans = max(ppre + nums[i], pre);
+            ppre = pre, pre = ans;
+        }
+        return ans;
+    }
+
     int rob(vector<int>& nums) {
-        if (nums.empty()) return 0;
         int n = nums.size();
-        if (n == 1) return nums[0];
-        // 1 to n-1
-        int a_ans = nums[0], a_pre = nums[0], a_ppre = 0;
-        for (int i = 1; i < n - 1; ++i) {
-            a_ans = max(a_ppre + nums[i], a_pre);
-            a_ppre = a_pre, a_pre = a_ans;
+        if (n == 1) {
+            return nums[0];
         }
-        // 2 to n
-        int b_ans = nums[1], b_pre = nums[1], b_ppre = 0;
-        for (int i = 2; i < n; ++i) {
-            b_ans = max(b_ppre + nums[i], b_pre);
-            b_ppre = b_pre, b_pre = b_ans;
-        }
-        return max(a_ans, b_ans);
+        return max(helper(nums, 0, n - 2), helper(nums, 1, n - 1));
     }
 };
