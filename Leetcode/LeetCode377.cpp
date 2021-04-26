088e483e742205a8a2621084b333f08b64ec103e
Ginakira <ginakira@foxmail.com>
2021-04-26 20:44:11 +0800

    LC377

---
+++ Leetcode/LeetCode377.cpp
@@ -0,0 +1,20 @@
+// LeetCode 377 组合总和IV
+#include <vector>
+using namespace std;
+
+// dp[i] 代表当目标为i时的方案总数，初始状态dp[0]=1
+class Solution {
+   public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= target; ++i) {
+            for (int num : nums) {
+                if (num <= i && dp[i - num] < INT_MAX - dp[i]) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
