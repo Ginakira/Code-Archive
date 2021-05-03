3006f38f57e9fc58673e2c0a95c3664b1bdc3b5a
Ginakira <ginakira@foxmail.com>
2021-05-03 21:14:31 +0800

    LC137S2

--- Leetcode/LeetCode137.cpp
+++ Leetcode/LeetCode137.cpp
@@ -18,3 +18,21 @@ class Solution {
         return mp.begin()->first;
     }
 };
+
+// 分别确定每一个二进制位 8ms 9.2MB
+class Solution2 {
+   public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            int total = 0;
+            for (int& num : nums) {
+                total += (num >> i) & 1;
+            }
+            if (total % 3) {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+    }
+};
