125f51dd46a9311158db4895efd9e67413b72c31
Ginakira <ginakira@foxmail.com>
2021-05-15 08:59:35 +0800

    LC 13

--- Leetcode/Leetcode13.cpp
+++ Leetcode/Leetcode13.cpp
@@ -1,39 +1,26 @@
 // 罗马数字转整数
 // 如果当前位后面的位权比前面高 则减去当前位 否则加上当前位
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
+unordered_map<char, int> romanToDec{{'I', 1},   {'V', 5},   {'X', 10},
+                                    {'L', 50},  {'C', 100}, {'D', 500},
+                                    {'M', 1000}};
 
 class Solution {
    public:
-    int getVal(char c) {
-        switch (c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-        }
-        return 0;
-    }
-
     int romanToInt(string s) {
-        int num = 0;
-        for (int i = 0; s[i]; ++i) {
-            if (s[i + 1] == 0 || getVal(s[i]) >= getVal(s[i + 1])) {
-                num += getVal(s[i]);
+        int n = s.size();
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            int value = romanToDec[s[i]];
+            if (i < n - 1 && value < romanToDec[s[i + 1]]) {
+                result -= value;
             } else {
-                num -= getVal(s[i]);
+                result += value;
             }
         }
-        return num;
+        return result;
     }
 };
