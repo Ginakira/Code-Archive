bd9753ace6c79d3bdf1776f3f0355337b31f10c3
Ginakira <ginakira@foxmail.com>
2021-06-08 22:33:09 +0800

    LC494

--- Leetcode/LeetCode494.cpp
+++ Leetcode/LeetCode494.cpp
@@ -1,8 +1,8 @@
 // LeetCode 494 目标和
 #include <algorithm>
 #include <numeric>
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
@@ -26,5 +26,30 @@ class Solution {
     int findTargetSumWays(vector<int>& nums, int target) {
         backtrack(nums, target, 0, 0);
         return count;
+    }
+};
+
+// 动态规划
+class Solution2 {
+   public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0);
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int neg = diff / 2;
+        vector<vector<int>> dp(n + 1, vector<int>(neg + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= neg; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[n][neg];
     }
 };
