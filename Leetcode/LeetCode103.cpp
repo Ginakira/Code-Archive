74bba0182446164b94fb1498bae2a2fff4eea629
Ginakira <ginakira@foxmail.com>
2021-07-06 00:14:44 +0800

    LC103S2

--- Leetcode/LeetCode103.cpp
+++ Leetcode/LeetCode103.cpp
@@ -1,5 +1,7 @@
 // LeetCode 103 二叉树的锯齿形层序遍历
 #include <algorithm>
+#include <deque>
+#include <queue>
 #include <vector>
 using namespace std;
 
@@ -31,5 +33,41 @@ class Solution {
         pushNodes(nodes, root->left, depth + 1);
         pushNodes(nodes, root->right, depth + 1);
         return;
+    }
+};
+
+// Solution2: BFS + 双端队列
+class Solution2 {
+   public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root) {
+        if (!root) return {};
+
+        queue<TreeNode *> que;
+        vector<vector<int>> result;
+        que.push(root);
+
+        bool is_normal_order = true;
+        while (!que.empty()) {
+            deque<int> dq;
+            int node_count = que.size();
+            for (int i = 0; i < node_count; ++i) {
+                TreeNode *node = que.front();
+                que.pop();
+                if (is_normal_order) {
+                    dq.push_back(node->val);
+                } else {
+                    dq.push_front(node->val);
+                }
+                if (node->left) {
+                    que.push(node->left);
+                }
+                if (node->right) {
+                    que.push(node->right);
+                }
+            }
+            is_normal_order = !is_normal_order;
+            result.emplace_back(dq.begin(), dq.end());
+        }
+        return result;
     }
 };
