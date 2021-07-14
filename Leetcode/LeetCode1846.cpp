6afcdca0fdb1b8ce9e16a8319a39f6803046bc01
Ginakira <ginakira@foxmail.com>
2021-07-15 01:15:33 +0800

    LC1846

---
+++ Leetcode/LeetCode1846.cpp
@@ -0,0 +1,23 @@
+// LeetCode 1846 减小和重新排列数组后的最大元素
+
+#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        arr[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            if (arr[i] - arr[i - 1] > 1) {
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+        return arr[n - 1];
+    }
+};
