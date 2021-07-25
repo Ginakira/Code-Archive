37d6b225ba9a6edfd239968719dfe6fcb1e0a6d4
Ginakira <ginakira@foxmail.com>
2021-07-25 15:21:42 +0800

    LC395

--- Leetcode/LeetCode395.cpp
+++ Leetcode/LeetCode395.cpp
@@ -1,60 +1,45 @@
 // LeetCode 395 至少有K个重复字符的最长子串
+#include <sstream>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 using namespace std;
 
 // 分治法
 // 以区间内出现次数小于k的字符为分割点，求各段的答案，取最大值
 class Solution {
-   public:
-    int longestSubstring(string s, int k) {
-        int n = s.size();
-        if (k == 1) {
-            return n;
+   private:
+    void split_string(const string &s, vector<string> &vec,
+                      const char flag = ' ') {
+        vec.clear();
+        stringstream ss(s);
+        string sub;
+        while (getline(ss, sub, flag)) {
+            vec.emplace_back(sub);
         }
-        if (n < k) {
-            return 0;
-        }
-        return dfs(s, 0, n - 1, k);
     }
 
-    int dfs(const string &s, int start, int end, int k) {
-        if (end <= start || end - start + 1 < k) {
-            return 0;
-        }
-        vector<int> count(26, 0);
-        for (int i = start; i <= end; ++i) {
-            ++count[s[i] - 'a'];
-        }
-
-        bool need_split = false;
-        for (const int &cnt : count) {
-            if (cnt > 0 && cnt < k) {
-                need_split = true;
-                break;
-            }
-        }
-        if (!need_split) {
-            return end - start + 1;
+   public:
+    int longestSubstring(string s, int k) {
+        if (s.size() < k) return 0;
+        unordered_set<char> chars(s.begin(), s.end());
+        unordered_map<char, int> counter;
+        for (const char &c : s) {
+            ++counter[c];
         }
 
-        int ret = 0;
-        int left = start, right = start;
-        while (right <= end) {
-            while (left <= end) {
-                if (count[s[left] - 'a'] >= k) break;
-                ++left;
+        for (char c : chars) {
+            if (counter[c] < k) {
+                int result = 0;
+                vector<string> sub_strings;
+                split_string(s, sub_strings, c);
+                for (const string &sub : sub_strings) {
+                    result = max(result, longestSubstring(sub, k));
+                }
+                return result;
             }
-            right = left + 1;
-            while (right <= end) {
-                if (count[s[right] - 'a'] < k) break;
-                ++right;
-            }
-            if (right - left > ret) {
-                ret = max(ret, dfs(s, left, right - 1, k));
-            }
-            left = right + 1;
         }
-        return ret;
+        return s.size();
     }
 };
