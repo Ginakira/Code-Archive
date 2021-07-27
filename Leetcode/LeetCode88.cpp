a544f36fc29414c6f7ef632e81bc095084c5c5c8
Ginakira <ginakira@foxmail.com>
2021-07-28 00:27:03 +0800

    LC88

--- Leetcode/LeetCode88.cpp
+++ Leetcode/LeetCode88.cpp
@@ -31,3 +31,18 @@ class Solution2 {
         return;
     }
 };
+
+class Solution3 {
+   public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int pa = m - 1, pb = n - 1, p = nums1.size() - 1;
+        while (pa >= 0 || pb >= 0) {
+            if (pb < 0 || (pa >= 0 && nums1[pa] > nums2[pb])) {
+                nums1[p--] = nums1[pa--];
+            } else {
+                nums1[p--] = nums2[pb--];
+            }
+        }
+        return;
+    }
+};
