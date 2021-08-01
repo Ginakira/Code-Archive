49fd878caaa02c1092241b4927aba22fdff26f10
Ginakira <ginakira@foxmail.com>
2021-08-01 12:21:15 +0800

    LC73

--- Leetcode/LeetCode73.cpp
+++ Leetcode/LeetCode73.cpp
@@ -1,4 +1,5 @@
 // LeetCode 73 矩阵置零
+#include <set>
 #include <vector>
 using namespace std;
 
@@ -7,31 +8,43 @@ class Solution {
    public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        bool row_zero = false, col_zero = false;
+        // 使用第一行第一列标记此行此列是否需要置零
+        bool row_zero = false,
+             col_zero = false;  // 第一行和第一列本身是否需要置零
+
         for (int i = 0; i < n; ++i) {
-            if (!matrix[i][0]) {
+            if (matrix[i][0] == 0) {
                 col_zero = true;
                 break;
             }
         }
         for (int j = 0; j < m; ++j) {
-            if (!matrix[0][j]) {
+            if (matrix[0][j] == 0) {
                 row_zero = true;
                 break;
             }
         }
+
+        // 使用第一行和第一列记录是否置零
         for (int i = 1; i < n; ++i) {
             for (int j = 1; j < m; ++j) {
-                if (!matrix[i][j]) {
+                if (matrix[i][j] == 0) {
                     matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
+        // 使用记录置零
         for (int i = 1; i < n; ++i) {
             for (int j = 1; j < m; ++j) {
-                if (!matrix[i][0] || !matrix[0][j]) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        // 处理首行首列
+        if (row_zero) {
+            for (int j = 0; j < m; ++j) {
+                matrix[0][j] = 0;
             }
         }
         if (col_zero) {
@@ -39,11 +52,31 @@ class Solution {
                 matrix[i][0] = 0;
             }
         }
-        if (row_zero) {
+    }
+};
+
+// set记录
+class Solution2 {
+   public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        set<pair<int, int>> st;
+        for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                matrix[0][j] = 0;
+                if (matrix[i][j] == 0) {
+                    st.insert(pair<int, int>{i, j});
+                }
             }
         }
-        return;
+
+        for (auto& pr : st) {
+            auto& [r, c] = pr;
+            for (int i = 0; i < n; ++i) {
+                matrix[i][c] = 0;
+            }
+            for (int j = 0; j < m; ++j) {
+                matrix[r][j] = 0;
+            }
+        }
     }
 };
