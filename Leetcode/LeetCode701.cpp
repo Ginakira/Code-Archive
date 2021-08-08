3bd18ce25bfda16e8411e33215b4a19682387036
Ginakira <ginakira@foxmail.com>
2021-08-08 23:29:41 +0800

    LC701

---
+++ Leetcode/LeetCode701.cpp
@@ -0,0 +1,31 @@
+// LeetCode 701 二叉搜索树中的插入操作
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    TreeNode *insertIntoBST(TreeNode *root, int val) {
+        if (root == nullptr) return new TreeNode(val);
+        if (val < root->val) {
+            root->left = insertIntoBST(root->left, val);
+        } else {
+            root->right = insertIntoBST(root->right, val);
+        }
+        return root;
+    }
+};
