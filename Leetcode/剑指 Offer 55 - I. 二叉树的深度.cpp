907d96005b46fcee645e6479596905c8d2d43f22
Ginakira <ginakira@foxmail.com>
2021-08-12 12:32:09 +0800

    OF55

---
+++ Leetcode/剑指 Offer 55 - I. 二叉树的深度.cpp
@@ -0,0 +1,23 @@
+// 剑指 Offer 55 - I. 二叉树的深度
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+   public:
+    int maxDepth(TreeNode *root) {
+        if (root == nullptr) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
