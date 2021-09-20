1bcca679084f31eeba1139c62292297671f1f3ff
Ginakira <ginakira@foxmail.com>
2021-09-20 17:13:46 +0800

    LC78

--- Leetcode/LeetCode718.cpp
+++ Leetcode/LeetCode718.cpp
@@ -13,9 +13,11 @@ class Solution {
         int n = nums1.size(), m = nums2.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         int max_len = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = m - 1; j >= 0; --j) {
-                dp[i][j] = (nums1[i] == nums2[j] ? dp[i + 1][j + 1] + 1 : 0);
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
                 max_len = max(max_len, dp[i][j]);
             }
         }
