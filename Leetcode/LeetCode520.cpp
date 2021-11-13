1cc7d92b293e2d0531bf41cd3e8e2deb87622880
Ginakira <ginakira@foxmail.com>
2021-11-13 15:58:12 +0800

    LC520

--- Leetcode/LeetCode520.cpp
+++ Leetcode/LeetCode520.cpp
@@ -1,4 +1,6 @@
 // LeetCode 520 检测大写字母
+#include <algorithm>
+#include <cctype>
 #include <string>
 using namespace std;
 
@@ -15,5 +17,18 @@ class Solution {
         }
         if (lowerCnt == n || upperCnt == n) return true;
         return upperCnt == 1 && isupper(word[0]);
+    }
+};
+
+class Solution2 {
+   public:
+    bool detectCapitalUse(string word) {
+        return (all_of(word.begin(), word.end(),
+                       [](char ch) { return (bool)islower(ch); }) ||
+                all_of(word.begin(), word.end(),
+                       [](char ch) { return (bool)isupper(ch); }) ||
+                (isupper(word[0]) &&
+                 all_of(word.begin() + 1, word.end(),
+                        [](char ch) { return (bool)islower(ch); })));
     }
 };
