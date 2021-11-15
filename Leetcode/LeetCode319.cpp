c84490f6bf23a5666cff3ae0a16c269ce734ca72
Ginakira <ginakira@foxmail.com>
2021-11-15 22:42:12 +0800

    LC319

---
+++ Leetcode/LeetCode319.cpp
@@ -0,0 +1,14 @@
+// LeetCode 319 灯泡开关
+
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int bulbSwitch(int n) { return floor(sqrt(n)); }
+};
