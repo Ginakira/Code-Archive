eb893aa6413d3bc617d48c89cd6f95691f63d29a
Ginakira <ginakira@foxmail.com>
2022-01-25 21:59:49 +0800

    LC1688

---
+++ Leetcode/LeetCode1688.cpp
@@ -0,0 +1,13 @@
+// LeetCode 1688 比赛中的配对次数
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int numberOfMatches(int n) { return n - 1; }
+};
