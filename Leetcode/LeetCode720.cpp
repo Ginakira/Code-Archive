103c0b8afe6bd102a1c26a9eb4d3feee08b69400
Ginakira <ginakira@foxmail.com>
2022-03-17 23:01:18 +0800

    LC720

--- Leetcode/LeetCode720.cpp
+++ Leetcode/LeetCode720.cpp
@@ -10,10 +10,10 @@ using namespace std;
 
 class Solution {
    public:
-    string longestWord(vector<string>& words) {
+    string longestWord(vector<string> &words) {
         unordered_set<string> st(words.begin(), words.end());
         string ans;
-        for (const string& word : words) {
+        for (const string &word : words) {
             if (word.size() < ans.size()) continue;
             bool valid = true;
             for (int i = 1; i < word.size(); ++i) {
@@ -24,6 +24,36 @@ class Solution {
             }
             if (valid && (word.size() > ans.size() || word < ans)) {
                 ans = word;
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution2 {
+   public:
+    string longestWord(vector<string> &words) {
+        sort(words.begin(), words.end(), [](auto &a, auto &b) {
+            return (a.size() == b.size()) ? (a < b) : (a.size() > b.size());
+        });
+
+        unordered_set<string> st;
+        for (auto &word : words) {
+            st.insert(word);
+        }
+
+        string ans;
+        for (auto &word : words) {
+            bool valid = true;
+            for (int i = 1; i < word.size(); ++i) {
+                if (st.find(word.substr(0, i)) == st.end()) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                ans = word;
+                break;
             }
         }
         return ans;
