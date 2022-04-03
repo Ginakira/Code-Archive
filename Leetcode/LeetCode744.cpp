39101bcdb6f3c6f33929783e43d2d2a130f0edcb
Ginakira <ginakira@foxmail.com>
2022-04-03 21:04:05 +0800

    LC744

---
+++ Leetcode/LeetCode744.cpp
@@ -0,0 +1,17 @@
+// LeetCode 744 寻找比目标字母大的最小字母
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        sort(letters.begin(), letters.end());
+        auto it = upper_bound(letters.begin(), letters.end(), target);
+        return it == letters.end() ? letters[0] : *it;
+    }
+};
