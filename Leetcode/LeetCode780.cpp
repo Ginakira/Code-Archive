9d187e96b9d8dca1e641dd60abe54df335447d53
Ginakira <ginakira@foxmail.com>
2022-04-10 00:04:08 +0800

    LC780

---
+++ Leetcode/LeetCode780.cpp
@@ -0,0 +1,29 @@
+// LeetCode 780 到达终点
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while (tx > sx && ty > sy && tx != ty) {
+            if (tx > ty) {
+                tx %= ty;
+            } else {
+                ty %= tx;
+            }
+        }
+        if (tx == sx && ty == sy) {
+            return true;
+        } else if (tx == sx) {
+            return ty > sy && (ty - sy) % tx == 0;
+        } else if (ty == sy) {
+            return tx > sx && (tx - sx) % ty == 0;
+        }
+        return false;
+    }
+};
