97744813256efb6707775f982c6263d260a3e8e8
Ginakira <ginakira@foxmail.com>
2022-05-25 01:10:25 +0800

    LC965

---
+++ Leetcode/LeetCode965.cpp
@@ -0,0 +1,30 @@
+// LeetCode 965 单值二叉树
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    bool isUnivalTree(TreeNode *root) {
+        if (!root) return true;
+        if ((root->left && root->left->val != root->val) ||
+            (root->right && root->right->val != root->val)) {
+            return false;
+        }
+        return isUnivalTree(root->left) && isUnivalTree(root->right);
+    }
+};
