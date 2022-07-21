abdfcb6b981d250fec73698643fa64c72e806155
Ginakira <ginakira@foxmail.com>
2022-07-21 23:54:40 +0800

    LC814

---
+++ Leetcode/LeetCode814.cpp
@@ -0,0 +1,33 @@
+// LeetCode 814 二叉树剪枝
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    TreeNode *pruneTree(TreeNode *root) {
+        if (!root) {
+            return nullptr;
+        }
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (!root->left && !root->right && !root->val) {
+            return nullptr;
+        }
+        return root;
+    }
+};
