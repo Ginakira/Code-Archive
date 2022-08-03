2e9cd91081f49e9fd78b5e2a6e1319aae92a9ce0
Ginakira <ginakira@foxmail.com>
2022-08-03 23:52:51 +0800

    LC899

---
+++ Leetcode/LeetCode899.cpp
@@ -0,0 +1,30 @@
+// LeetCode 899 有序队列
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    string orderlyQueue(string s, int k) {
+        if (k == 1) {
+            string smallest = s;
+            int n = s.size();
+            for (int i = 1; i < n; i++) {
+                char c = s[0];
+                s = s.substr(1);
+                s.push_back(c);
+                if (s < smallest) {
+                    smallest = s;
+                }
+            }
+            return smallest;
+        } else {
+            sort(s.begin(), s.end());
+            return s;
+        }
+    }
+};
