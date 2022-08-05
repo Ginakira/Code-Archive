2ae7a60539dd6c5a26e8a3f83ae894609e9127e6
Ginakira <ginakira@foxmail.com>
2022-08-05 23:56:54 +0800

    LC623

---
+++ Leetcode/LeetCode623.cpp
@@ -0,0 +1,37 @@
+// LeetCode 623 在二叉树中增加一行
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    TreeNode *addOneRow(TreeNode *root, int val, int depth) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, nullptr);
+        } else if (depth == 2) {
+            root->left = new TreeNode(val, root->left, nullptr);
+            root->right = new TreeNode(val, nullptr, root->right);
+        } else {
+            root->left = addOneRow(root->left, val, depth - 1);
+            root->right = addOneRow(root->right, val, depth - 1);
+        }
+        return root;
+    }
+};
