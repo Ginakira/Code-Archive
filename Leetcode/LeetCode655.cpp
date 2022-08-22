b5a55a9bf988075920f605e808495a69a1069396
Ginakira <ginakira@foxmail.com>
2022-08-22 23:50:10 +0800

    LC655

---
+++ Leetcode/LeetCode655.cpp
@@ -0,0 +1,53 @@
+// LeetCode 655 输出二叉树
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   private:
+    int get_height(TreeNode* root) {
+        int h = 0;
+        if (root->left) {
+            h = max(h, get_height(root->left) + 1);
+        }
+        if (root->right) {
+            h = max(h, get_height(root->right) + 1);
+        }
+        return h;
+    }
+
+    void dfs(vector<vector<string>>& res, TreeNode* root, int r, int c,
+             const int& height) {
+        res[r][c] = to_string(root->val);
+        if (root->left) {
+            dfs(res, root->left, r + 1, c - (1 << (height - r - 1)), height);
+        }
+        if (root->right) {
+            dfs(res, root->right, r + 1, c + (1 << (height - r - 1)), height);
+        }
+    }
+
+   public:
+    vector<vector<string>> printTree(TreeNode* root) {
+        int height = get_height(root);
+        int m = height + 1;
+        int n = (1 << (height + 1)) - 1;
+        vector<vector<string>> res(m, vector<string>(n, ""));
+        dfs(res, root, 0, (n - 1) / 2, height);
+        return res;
+    }
+};
