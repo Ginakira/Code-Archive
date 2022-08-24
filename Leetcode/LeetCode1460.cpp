ab9aba8178c1262f4a791957be86fce6acbb0388
Ginakira <ginakira@foxmail.com>
2022-08-24 23:07:26 +0800

    LC1460

---
+++ Leetcode/LeetCode1460.cpp
@@ -0,0 +1,17 @@
+// LeetCode 1460 通过翻转子数组使两个数组相等
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        return target == arr;
+    }
+};
