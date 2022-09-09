a9c90bd4ed35d4b2d4685d428ac7920f5b47f4f1
Ginakira <ginakira@foxmail.com>
2022-09-10 00:09:30 +0800

    LC669

---
+++ Leetcode/LeetCode669.cpp
@@ -0,0 +1,36 @@
+// LeetCode 669 修剪二叉搜索树
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    TreeNode *trimBST(TreeNode *root, int low, int high) {
+        if (!root) {
+            return nullptr;
+        }
+        if (root->val < low) {
+            return trimBST(root->right, low, high);
+        } else if (root->val > high) {
+            return trimBST(root->left, low, high);
+        } else {
+            root->left = trimBST(root->left, low, high);
+            root->right = trimBST(root->right, low, high);
+        }
+        return root;
+    }
+};
