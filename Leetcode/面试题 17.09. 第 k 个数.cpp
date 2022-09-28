7048f3eb15c0c8513a0bf8f477f9c7ba03c06f1d
Ginakira <ginakira@foxmail.com>
2022-09-28 22:51:42 +0800

    ITW1709

---
+++ Leetcode/面试题 17.09. 第 k 个数.cpp
@@ -0,0 +1,33 @@
+// 面试题 17.09. 第 k 个数
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int getKthMagicNumber(int k) {
+        priority_queue<long, vector<long>, greater<long>> q;
+        unordered_set<long> seen;
+        array<int, 3> factors{3, 5, 7};
+
+        q.push(1);
+        seen.insert(1);
+        int ans = 1;
+        for (int _ = 0; _ < k; ++_) {
+            ans = q.top();
+            q.pop();
+            for (auto &factor : factors) {
+                long num = 1L * ans * factor;
+                if (!seen.count(num)) {
+                    q.push(num);
+                    seen.insert(num);
+                }
+            }
+        }
+        return ans;
+    }
+};
