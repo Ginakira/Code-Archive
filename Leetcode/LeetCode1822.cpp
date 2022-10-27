d7ea034bc4f48467562a13f09e69b6de7c761fc8
Ginakira <ginakira@foxmail.com>
2022-10-27 23:41:38 +0800

    LC1822

---
+++ Leetcode/LeetCode1822.cpp
@@ -0,0 +1,23 @@
+// LeetCode 1822 数组元素积的符号
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int arraySign(vector<int>& nums) {
+        int sign = 1;
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            } else if (num < 0) {
+                sign *= -1;
+            }
+        }
+        return sign;
+    }
+};
