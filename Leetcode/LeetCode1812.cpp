58c6a504d059cf923f01b57d09d8f6eb6f150cbe
Ginakira <ginakira@foxmail.com>
2022-12-08 22:49:19 +0800

    LC1812

---
+++ Leetcode/LeetCode1812.cpp
@@ -0,0 +1,16 @@
+// LeetCode 1812 判断国际象棋棋盘中一个格子的颜色
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    bool squareIsWhite(string coordinates) {
+        return (coordinates[0] - 'a' & 1) ? !(coordinates[1] - '1' & 1)
+                                          : (coordinates[1] - '1' & 1);
+    }
+};
