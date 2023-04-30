c4307f7a45caffd19c1481df6764f6cf993eaa89
Ginakira <ginakira@foxmail.com>
2023-04-30 21:49:38 +0800

    LC1033

---
+++ Leetcode/LeetCode1033.cpp
@@ -0,0 +1,32 @@
+// LeetCOde 1033 移动石子直到连续
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> numMovesStones(int a, int b, int c) {
+        int x = min({a, b, c});
+        int z = max({a, b, c});
+        int y = a + b + c - x - z;
+
+        vector<int> res(2, 0);
+        res[0] = 2;
+        if ((z - y) == 1 && (y - x) == 1) {
+            res[0] = 0;
+        } else if ((z - y) <= 2 || (y - x) <= 2) {
+            res[0] = 1;
+        }
+        res[1] = z - x - 2;
+        return res;
+    }
+};
