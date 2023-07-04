6b2127b26dda34ca2c7a4619d03602dd0172b781
Ginakira <ginakira@foxmail.com>
2023-07-05 00:04:54 +0800

    LC2600

---
+++ Leetcode/LeetCode2600.cpp
@@ -0,0 +1,26 @@
+// LeetCode 2600 K件物品的最大和
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if (k < numOnes) {
+            return k;
+        } else if (k <= numOnes + numZeros) {
+            return numOnes;
+        } else {
+            return numOnes - (k - numOnes - numZeros);
+        }
+    }
+};
