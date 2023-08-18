023e26b65360438951ffc6d2b5dda0a3d10e742d
Ginakira <ginakira@foxmail.com>
2023-08-19 00:15:38 +0800

    LC2235

---
+++ Leetcode/LeetCode2235.cpp
@@ -0,0 +1,18 @@
+// LeetCode 2235 两整数相加
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int sum(int num1, int num2) { return num1 + num2; }
+};
