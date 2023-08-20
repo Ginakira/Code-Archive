bc2f6d1895208445c6ee6a7885d01a7c14a03ed0
Ginakira <ginakira@foxmail.com>
2023-08-20 16:23:53 +0800

    LC2236

---
+++ Leetcode/LeetCode2236.cpp
@@ -0,0 +1,30 @@
+// LeetCode 2236 判断根节点是否等于子结点之和
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    bool checkTree(TreeNode *root) {
+        return root->val == root->left->val + root->right->val;
+    }
+};
