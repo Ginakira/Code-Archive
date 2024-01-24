fcfcb0de85785b676500dfb037a6d13fb0856529
Ginakira <ginakira@foxmail.com>
2024-01-24 22:34:53 +0800

    LC2865-S2

--- Leetcode/LeetCode2865.cpp
+++ Leetcode/LeetCode2865.cpp
@@ -34,3 +34,40 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+   public:
+    long long maximumSumOfHeights(vector<int>& maxHeights) {
+        int n = maxHeights.size();
+        long long ans = 0;
+        vector<long long> presum(n), sufsum(n);
+        stack<int> stk1, stk2;
+
+        for (int i = 0; i < n; ++i) {
+            while (!stk1.empty() && maxHeights[stk1.top()] > maxHeights[i]) {
+                stk1.pop();
+            }
+            if (stk1.empty()) {
+                presum[i] = 1LL * (i + 1) * maxHeights[i];
+            } else {
+                presum[i] =
+                    presum[stk1.top()] + 1LL * (i - stk1.top()) * maxHeights[i];
+            }
+            stk1.emplace(i);
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            while (!stk2.empty() && maxHeights[stk2.top()] > maxHeights[i]) {
+                stk2.pop();
+            }
+            if (stk2.empty()) {
+                sufsum[i] = 1LL * (n - i) * maxHeights[i];
+            } else {
+                sufsum[i] =
+                    sufsum[stk2.top()] + 1LL * (stk2.top() - i) * maxHeights[i];
+            }
+            stk2.emplace(i);
+            ans = max(ans, presum[i] + sufsum[i] - maxHeights[i]);
+        }
+        return ans;
+    }
+};
