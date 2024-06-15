b0b4d8883d0cee238f4326576a27634cc1504410
Ginakira <ginakira@foxmail.com>
2024-06-16 01:19:44 +0800

    LC521

--- Leetcode/LeetCode521.cpp
+++ Leetcode/LeetCode521.cpp
@@ -9,7 +9,6 @@ using namespace std;
 class Solution {
    public:
     int findLUSlength(string a, string b) {
-        if (a == b) return -1;
-        return max(a.size(), b.size());
+        return a == b ? -1 : max(a.size(), b.size());
     }
 };
