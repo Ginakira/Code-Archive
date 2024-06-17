6fb7b610dccf0474b3dd8eedadcd35c065be4762
Ginakira <ginakira@foxmail.com>
2024-06-17 23:26:28 +0800

    LC522

---
+++ Leetcode/LeetCode522.cpp
@@ -0,0 +1,45 @@
+// LeetCode 522 最长特殊序列II
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int findLUSlength(vector<string> &strs) {
+        auto is_subseq = [](const string &s, const string &t) {
+            int pt_s = 0, pt_t = 0;
+            while (pt_s < s.size() && pt_t < t.size()) {
+                if (s[pt_s] == t[pt_t]) {
+                    ++pt_s;
+                }
+                ++pt_t;
+            }
+            return pt_s == s.size();
+        };
+
+        int n = strs.size();
+        int ans = -1;
+        for (int i = 0; i < n; ++i) {
+            bool check = true;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && is_subseq(strs[i], strs[j])) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                ans = max(ans, static_cast<int>(strs[i].size()));
+            }
+        }
+        return ans;
+    }
+};
