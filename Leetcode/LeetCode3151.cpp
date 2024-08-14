45b105b94635f4940330258b08535f142a477da9
Ginakira <ginakira@foxmail.com>
2024-08-15 01:37:19 +0800

    LC3151

--- Leetcode/LeetCode3151.cpp
+++ Leetcode/LeetCode3151.cpp
@@ -17,7 +17,7 @@ class Solution {
     bool isArraySpecial(vector<int>& nums) {
         int n = nums.size();
         for (int i = 1; i < n; ++i) {
-            if (!((nums[i - 1] & 1) ^ (nums[i] & 1))) {
+            if (!((nums[i - 1] ^ nums[i]) & 1)) {
                 return false;
             }
         }
