e7c8c4dac8c262f8e5a8956957a1dc5d97371b20
Ginakira <ginakira@foxmail.com>
2024-11-18 23:22:33 +0800

    LC661

--- Leetcode/LeetCode661.cpp
+++ Leetcode/LeetCode661.cpp
@@ -42,3 +42,32 @@ class Solution {
         return result;
     }
 };
+
+class Solution2 {
+   public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int n = img.size(), m = img[0].size();
+        vector<vector<int>> sum(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                sum[i][j] = img[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] -
+                            sum[i - 1][j - 1];
+            }
+        }
+
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                int a = max(0, i - 1);      // up
+                int b = max(0, j - 1);      // left
+                int c = min(n - 1, i + 1);  // down
+                int d = min(m - 1, j + 1);  // right
+                int cnt = (c - a + 1) * (d - b + 1);
+                int total = sum[c + 1][d + 1] - sum[a][d + 1] - sum[c + 1][b] +
+                            sum[a][b];
+                ans[i][j] = total / cnt;
+            }
+        }
+        return ans;
+    }
+};
