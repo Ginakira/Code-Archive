455dc65fe7ca9546b2d4650f9b858fbaa9884bab
Ginakira <ginakira@foxmail.com>
2025-04-14 23:06:24 +0800

    LC1534

---
+++ Leetcode/LeetCode1534.cpp
@@ -0,0 +1,33 @@
+// LeetCode 1534 统计好三元组
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int cnt = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    if (abs(arr[i] - arr[j]) <= a &&
+                        abs(arr[j] - arr[k]) <= b &&
+                        abs(arr[i] - arr[k]) <= c) {
+                        ++cnt;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
