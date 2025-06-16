1ab434ff6939c7a275d8cea7b6c960b20eb0c16c
Ginakira <ginakira@foxmail.com>
2025-06-16 23:21:10 +0800

    LC2016

--- Leetcode/LeetCode2016.cpp
+++ Leetcode/LeetCode2016.cpp
@@ -9,35 +9,50 @@ using namespace std;
 
 // 暴力
 class Solution {
-   public:
-    int maximumDifference(vector<int>& nums) {
-        int max_diff = 0;
-        size_t n = nums.size();
-        for (size_t i = 0; i < n; ++i) {
-            for (size_t j = i + 1; j < n; ++j) {
-                int diff = nums[j] - nums[i];
-                if (diff > max_diff) {
-                    max_diff = diff;
-                }
-            }
+public:
+  int maximumDifference(vector<int> &nums) {
+    int max_diff = 0;
+    size_t n = nums.size();
+    for (size_t i = 0; i < n; ++i) {
+      for (size_t j = i + 1; j < n; ++j) {
+        int diff = nums[j] - nums[i];
+        if (diff > max_diff) {
+          max_diff = diff;
         }
-        return max_diff == 0 ? -1 : max_diff;
+      }
     }
+    return max_diff == 0 ? -1 : max_diff;
+  }
 };
 
 // 最小前缀
 class Solution2 {
-   public:
-    int maximumDifference(vector<int>& nums) {
-        int ans = -1, pre_min = nums[0];
-        size_t n = nums.size();
-        for (size_t i = 1; i < n; ++i) {
-            if (nums[i] > pre_min) {
-                ans = max(ans, nums[i] - pre_min);
-            } else {
-                pre_min = nums[i];
-            }
-        }
-        return ans;
+public:
+  int maximumDifference(vector<int> &nums) {
+    int ans = -1, pre_min = nums[0];
+    size_t n = nums.size();
+    for (size_t i = 1; i < n; ++i) {
+      if (nums[i] > pre_min) {
+        ans = max(ans, nums[i] - pre_min);
+      } else {
+        pre_min = nums[i];
+      }
     }
+    return ans;
+  }
+};
+
+class Solution3 {
+public:
+  int maximumDifference(vector<int> &nums) {
+    int ans = -1;
+    int min_val = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+      if (nums[i] > min_val) {
+        ans = max(ans, nums[i] - min_val);
+      }
+      min_val = min(min_val, nums[i]);
+    }
+    return ans;
+  }
 };
