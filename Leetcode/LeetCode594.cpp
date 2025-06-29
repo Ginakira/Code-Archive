1cbafd2ca64458a052e2d09c7fe8ee1e33f4817f
Ginakira <ginakira@foxmail.com>
2025-06-30 01:01:25 +0800

    LC594

--- Leetcode/LeetCode594.cpp
+++ Leetcode/LeetCode594.cpp
@@ -8,39 +8,56 @@
 using namespace std;
 
 class Solution {
-   public:
-    int findLHS(vector<int> &nums) {
-        unordered_map<int, int> mp;
-        for (const int &num : nums) {
-            mp[num]++;
-        }
-
-        int max_len = 0;
-        for (const int &num : nums) {
-            if (mp.find(num + 1) != mp.end()) {
-                max_len = max(mp[num] + mp[num + 1], max_len);
-            }
-        }
-        return max_len;
+ public:
+  int findLHS(vector<int> &nums) {
+    unordered_map<int, int> mp;
+    for (const int &num : nums) {
+      mp[num]++;
     }
+
+    int max_len = 0;
+    for (const int &num : nums) {
+      if (mp.find(num + 1) != mp.end()) {
+        max_len = max(mp[num] + mp[num + 1], max_len);
+      }
+    }
+    return max_len;
+  }
 };
 
 // 一趟遍历
 class Solution2 {
-   public:
-    int findLHS(vector<int> &nums) {
-        unordered_map<int, int> mp;
+ public:
+  int findLHS(vector<int> &nums) {
+    unordered_map<int, int> mp;
 
-        int max_len = 0;
-        for (const int &num : nums) {
-            ++mp[num];
-            if (mp.find(num - 1) != mp.end()) {
-                max_len = max(mp[num] + mp[num - 1], max_len);
-            }
-            if (mp.find(num + 1) != mp.end()) {
-                max_len = max(mp[num] + mp[num + 1], max_len);
-            }
-        }
-        return max_len;
+    int max_len = 0;
+    for (const int &num : nums) {
+      ++mp[num];
+      if (mp.find(num - 1) != mp.end()) {
+        max_len = max(mp[num] + mp[num - 1], max_len);
+      }
+      if (mp.find(num + 1) != mp.end()) {
+        max_len = max(mp[num] + mp[num + 1], max_len);
+      }
     }
+    return max_len;
+  }
+};
+
+class Solution3 {
+ public:
+  int findLHS(vector<int> &nums) {
+    unordered_map<int, int> mp;
+    for (int num : nums) {
+      ++mp[num];
+    }
+    int ans = 0;
+    for (auto &[num, cnt] : mp) {
+      if (mp.contains(num + 1)) {
+        ans = max(ans, cnt + mp[num + 1]);
+      }
+    }
+    return ans;
+  }
 };
