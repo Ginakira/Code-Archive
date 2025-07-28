b7b50a9e0261b8cefe9041aa14059c4dba7166b1
Ginakira <ginakira@foxmail.com>
2025-07-28 21:06:56 +0800

    LC2044

--- Leetcode/LeetCode2044.cpp
+++ Leetcode/LeetCode2044.cpp
@@ -9,23 +9,47 @@ using namespace std;
 
 // 状态压缩
 class Solution {
-   public:
-    int countMaxOrSubsets(vector<int>& nums) {
-        int n = nums.size(), max_ans = 0, cnt = 0, state = 1 << n;
-        for (int i = 0; i < state; ++i) {
-            int result = 0;
-            for (int j = 0; j < n; ++j) {
-                if ((i >> j) & 1) {
-                    result |= nums[j];
-                }
-            }
-            if (result == max_ans) {
-                ++cnt;
-            } else if (result > max_ans) {
-                max_ans = result;
-                cnt = 1;
-            }
+ public:
+  int countMaxOrSubsets(vector<int>& nums) {
+    int n = nums.size(), max_ans = 0, cnt = 0, state = 1 << n;
+    for (int i = 0; i < state; ++i) {
+      int result = 0;
+      for (int j = 0; j < n; ++j) {
+        if ((i >> j) & 1) {
+          result |= nums[j];
         }
-        return cnt;
+      }
+      if (result == max_ans) {
+        ++cnt;
+      } else if (result > max_ans) {
+        max_ans = result;
+        cnt = 1;
+      }
     }
+    return cnt;
+  }
+};
+
+class Solution2 {
+ public:
+  int countMaxOrSubsets(vector<int>& nums) {
+    int total_or = std::reduce(nums.begin(), nums.end(), 0, std::bit_or());
+    int ans = 0;
+    int n = nums.size();
+
+    auto dfs = [&](this auto&& dfs, int i, int subset_or) -> void {
+      if (subset_or == total_or) {
+        ans += 1 << (n - i);
+        return;
+      }
+      if (i == n) {
+        return;
+      }
+      dfs(i + 1, subset_or);
+      dfs(i + 1, subset_or | nums[i]);
+    };
+
+    dfs(0, 0);
+    return ans;
+  }
 };
