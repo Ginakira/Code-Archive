98669eac5413ed552b934455437c9ad458a06c32
Ginakira <ginakira@foxmail.com>
2025-08-01 22:03:26 +0800

    LC118

--- Leetcode/LeetCode118.cpp
+++ Leetcode/LeetCode118.cpp
@@ -4,16 +4,31 @@ using namespace std;
 
 // Solution1 0ms
 class Solution {
-   public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result(numRows, vector<int>{1});
-        for (int row = 1; row < numRows; ++row) {
-            for (int ind = 1; ind < row; ++ind) {
-                result[row].push_back(result[row - 1][ind] +
-                                      result[row - 1][ind - 1]);
-            }
-            result[row].push_back(1);
-        }
-        return result;
+ public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> result(numRows, vector<int>{1});
+    for (int row = 1; row < numRows; ++row) {
+      for (int ind = 1; ind < row; ++ind) {
+        result[row].push_back(result[row - 1][ind] + result[row - 1][ind - 1]);
+      }
+      result[row].push_back(1);
     }
+    return result;
+  }
+};
+
+class Solution2 {
+ public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> ans;
+    ans.reserve(numRows);
+    for (int i = 0; i < numRows; ++i) {
+      vector<int> row(i + 1, 1);
+      for (int j = 1; j < i; ++j) {
+        row[j] = ans.back()[j - 1] + ans.back()[j];
+      }
+      ans.emplace_back(std::move(row));
+    }
+    return ans;
+  }
 };
