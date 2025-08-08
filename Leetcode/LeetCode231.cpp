4557c29ce1d4da5c3992956f31d14ddefbf6ce4a
Ginakira <ginakira@foxmail.com>
2025-08-09 00:20:42 +0800

    LC231

--- Leetcode/LeetCode231.cpp
+++ Leetcode/LeetCode231.cpp
@@ -1,6 +1,11 @@
 // LeetCode 231 2的幂
 
 class Solution {
-   public:
-    bool isPowerOfTwo(int n) { return (n > 0 && (n & (n - 1)) == 0); }
+ public:
+  bool isPowerOfTwo(int n) { return (n > 0 && (n & (n - 1)) == 0); }
+};
+
+class Solution2 {
+ public:
+  bool isPowerOfTwo(int n) { return n > 0 && __builtin_popcount(n) == 1; }
 };
