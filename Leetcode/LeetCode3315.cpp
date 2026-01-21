b928a60360a1ff02755f65eb4d8ccfd046e86eb1
Ginakira <ginakira@foxmail.com>
2026-01-21 23:31:15 +0800

    LC3315

---
+++ Leetcode/LeetCode3315.cpp
@@ -0,0 +1,29 @@
+// LeetCode 3315 构造最小位运算数组 II
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> minBitwiseArray(vector<int>& nums) {
+    for (int& x : nums) {
+      int res = -1;
+      int d = 1;
+      while ((x & d) != 0) {
+        res = x - d;
+        d <<= 1;
+      }
+      x = res;
+    }
+    return nums;
+  }
+};
