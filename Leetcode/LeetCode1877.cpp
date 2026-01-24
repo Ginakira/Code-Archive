f2ac5746ffdfbed4924c4645380afa4c655b4f9b
Ginakira <ginakira@foxmail.com>
2026-01-24 21:39:54 +0800

    LC1877

--- Leetcode/LeetCode1877.cpp
+++ Leetcode/LeetCode1877.cpp
@@ -8,15 +8,28 @@ using namespace std;
 
 // 排序 + 双指针
 class Solution {
-   public:
-    int minPairSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size(), left = 0, right = n - 1;
-        int max_val = 0;
-        while (left < right) {
-            max_val = max(max_val, nums[left] + nums[right]);
-            ++left, --right;
-        }
-        return max_val;
+ public:
+  int minPairSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size(), left = 0, right = n - 1;
+    int max_val = 0;
+    while (left < right) {
+      max_val = max(max_val, nums[left] + nums[right]);
+      ++left, --right;
     }
+    return max_val;
+  }
+};
+
+class Solution2 {
+ public:
+  int minPairSum(vector<int>& nums) {
+    ranges::sort(nums);
+    int ans = 0;
+    int n = nums.size();
+    for (int i = 0; i < n / 2; ++i) {
+      ans = max(ans, nums[i] + nums[n - i - 1]);
+    }
+    return ans;
+  }
 };
