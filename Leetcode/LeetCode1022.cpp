69b03eb05ab7d0acb589af3d9e3adbcef7dcb8c6
Ginakira <ginakira@foxmail.com>
2026-02-24 19:56:20 +0800

    LC1022S2

--- Leetcode/LeetCode1022.cpp
+++ Leetcode/LeetCode1022.cpp
@@ -8,39 +8,72 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right)
-        : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
-   private:
-    int sum_;
+ private:
+  int sum_;
 
-    void helper(TreeNode *root, int cur_num) {
-        if (root == nullptr) {
-            return;
-        }
-        cur_num <<= 1;
-        cur_num |= root->val;
-        if (root->left == nullptr && root->right == nullptr) {
-            sum_ += cur_num;
-        }
-        if (root->left) {
-            helper(root->left, cur_num);
-        }
-        if (root->right) {
-            helper(root->right, cur_num);
-        }
+  void helper(TreeNode *root, int cur_num) {
+    if (root == nullptr) {
+      return;
     }
+    cur_num <<= 1;
+    cur_num |= root->val;
+    if (root->left == nullptr && root->right == nullptr) {
+      sum_ += cur_num;
+    }
+    if (root->left) {
+      helper(root->left, cur_num);
+    }
+    if (root->right) {
+      helper(root->right, cur_num);
+    }
+  }
 
-   public:
-    int sumRootToLeaf(TreeNode *root) {
-        helper(root, 0);
-        return sum_;
-    }
+ public:
+  int sumRootToLeaf(TreeNode *root) {
+    helper(root, 0);
+    return sum_;
+  }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution2 {
+ public:
+  int sumRootToLeaf(TreeNode *root) {
+    int ans = 0;
+    auto dfs = [&](this auto &&dfs, TreeNode *root, int cur) {
+      if (!root) {
+        return;
+      }
+      cur = (cur << 1) | root->val;
+      if (!root->left && !root->right) {
+        ans += cur;
+        return;
+      }
+      dfs(root->left, cur);
+      dfs(root->right, cur);
+    };
+    dfs(root, 0);
+    return ans;
+  }
 };
