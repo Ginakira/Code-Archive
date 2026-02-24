8fbbd472cc0c7d3fa5f92f88519e1376a68006bc
Ginakira <ginakira@foxmail.com>
2026-02-25 00:39:01 +0800

    LC1356S3

--- Leetcode/LeetCode1356.cpp
+++ Leetcode/LeetCode1356.cpp
@@ -8,39 +8,50 @@ using std::vector;
 
 // 解法1：sort + 位运算
 class Solution {
-   public:
-    int hummingWeight(int num) {
-        int ret = 0;
-        while (num) {
-            if (num & 1) ret++;
-            num >>= 1;
-        }
-        return ret;
+ public:
+  int hummingWeight(int num) {
+    int ret = 0;
+    while (num) {
+      if (num & 1) ret++;
+      num >>= 1;
     }
+    return ret;
+  }
 
-    vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), [&](int a, int b) {
-            int num_a = hummingWeight(a), num_b = hummingWeight(b);
-            if (num_a == num_b) {
-                return a < b;
-            }
-            return num_a < num_b;
-        });
-        return arr;
-    }
+  vector<int> sortByBits(vector<int>& arr) {
+    sort(arr.begin(), arr.end(), [&](int a, int b) {
+      int num_a = hummingWeight(a), num_b = hummingWeight(b);
+      if (num_a == num_b) {
+        return a < b;
+      }
+      return num_a < num_b;
+    });
+    return arr;
+  }
 };
 
 // bitset
 class Solution2 {
-   public:
-    vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), [&](int a, int b) {
-            int num_a = bitset<32>(a).count(), num_b = bitset<32>(b).count();
-            if (num_a == num_b) {
-                return a < b;
-            }
-            return num_a < num_b;
-        });
-        return arr;
-    }
+ public:
+  vector<int> sortByBits(vector<int>& arr) {
+    sort(arr.begin(), arr.end(), [&](int a, int b) {
+      int num_a = bitset<32>(a).count(), num_b = bitset<32>(b).count();
+      if (num_a == num_b) {
+        return a < b;
+      }
+      return num_a < num_b;
+    });
+    return arr;
+  }
+};
+
+class Solution3 {
+ public:
+  vector<int> sortByBits(vector<int>& arr) {
+    ranges::sort(arr, [](int a, int b) {
+      int cnt_a = __builtin_popcount(a), cnt_b = __builtin_popcount(b);
+      return cnt_a == cnt_b ? a < b : cnt_a < cnt_b;
+    });
+    return arr;
+  }
 };
