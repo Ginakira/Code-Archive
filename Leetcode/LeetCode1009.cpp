5010fe723115ccdec851a22f7c6f0fafb3a18416
Ginakira <ginakira@foxmail.com>
2026-03-11 23:36:50 +0800

    LC1009

--- Leetcode/LeetCode1009.cpp
+++ Leetcode/LeetCode1009.cpp
@@ -8,13 +8,24 @@
 using namespace std;
 
 class Solution {
-   public:
-    int bitwiseComplement(int n) {
-        int base = 1;
-        while (base < n) {
-            base <<= 1;
-            base += 1;
-        }
-        return base ^ n;
+ public:
+  int bitwiseComplement(int n) {
+    int base = 1;
+    while (base < n) {
+      base <<= 1;
+      base += 1;
     }
+    return base ^ n;
+  }
+};
+
+class Solution2 {
+ public:
+  int bitwiseComplement(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    int cnt = std::countl_zero((uint32_t)n);
+    return (~n) & (~0u >> cnt);
+  }
 };
