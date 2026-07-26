cf03b7dc25726801d4b1a23d5f50fbcb2f34284b
Ginakira <ginakira@foxmail.com>
2026-07-26 21:51:28 +0800

    LC628

--- Leetcode/LeetCode628.cpp
+++ Leetcode/LeetCode628.cpp
@@ -3,11 +3,21 @@
 using namespace std;
 
 class Solution {
-   public:
-    int maximumProduct(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.rbegin(), nums.rend());
-        return max(nums[0] * nums[1] * nums[2],
-                   nums[0] * nums[n - 1] * nums[n - 2]);
-    }
+ public:
+  int maximumProduct(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.rbegin(), nums.rend());
+    return max(nums[0] * nums[1] * nums[2],
+               nums[0] * nums[n - 1] * nums[n - 2]);
+  }
+};
+
+class Solution2 {
+ public:
+  int maximumProduct(vector<int>& nums) {
+    int n = nums.size();
+    ranges::sort(nums, greater<int>());
+    return max(
+        {nums[0] * nums[1] * nums[2], nums[0] * nums[n - 1] * nums[n - 2]});
+  }
 };
