2e5108d60e4364e8b7c8418af9916cf9a8f11eae
Ginakira <ginakira@foxmail.com>
2026-07-27 23:27:40 +0800

    LC1464

--- Leetcode/LeetCode1464.cpp
+++ Leetcode/LeetCode1464.cpp
@@ -8,9 +8,18 @@
 using namespace std;
 
 class Solution {
-   public:
-    int maxProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), greater<int>());
-        return (nums[0] - 1) * (nums[1] - 1);
-    }
+ public:
+  int maxProduct(vector<int>& nums) {
+    sort(nums.begin(), nums.end(), greater<int>());
+    return (nums[0] - 1) * (nums[1] - 1);
+  }
+};
+
+class Solution2 {
+ public:
+  int maxProduct(vector<int>& nums) {
+    int n = nums.size();
+    ranges::sort(nums);
+    return (nums[n - 1] - 1) * (nums[n - 2] - 1);
+  }
 };
