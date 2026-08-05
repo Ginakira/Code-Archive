a6df0667704849c9b72db4f7fd260eae6848d9da
Ginakira <ginakira@foxmail.com>
2026-08-05 23:46:07 +0800

    LC3310

--- Leetcode/LeetCode3310.cpp
+++ Leetcode/LeetCode3310.cpp
@@ -3,19 +3,16 @@
 class Solution {
  public:
   vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-    vector<vector<int>> callees(n), callers(n);
+    vector<vector<int>> callees(n);
     vector<int8_t> bad_funcs(n, false);
     for (auto& inv : invocations) {
       int caller = inv[0], callee = inv[1];
       callees[caller].emplace_back(callee);
-      callers[callee].emplace_back(caller);
     }
-    vector<int8_t> seen(n, false);
     auto dfs = [&](this auto& dfs, int cur) {
-      if (seen[cur]) {
+      if (bad_funcs[cur]) {
         return;
       }
-      seen[cur] = true;
       bad_funcs[cur] = true;
       for (int callee : callees[cur]) {
         dfs(callee);
@@ -23,17 +20,19 @@ class Solution {
     };
     dfs(k);
 
+    for (auto& inv : invocations) {
+      int caller = inv[0], callee = inv[1];
+      if (bad_funcs[callee] && !bad_funcs[caller]) {
+        vector<int> ans(n);
+        iota(ans.begin(), ans.end(), 0);
+        return ans;
+      }
+    }
+
     vector<int> ans;
     ans.reserve(n);
     for (int i = 0; i < n; ++i) {
       if (bad_funcs[i]) {
-        for (int caller : callers[i]) {
-          if (!bad_funcs[caller]) {
-            ans.resize(n);
-            iota(ans.begin(), ans.end(), 0);
-            return ans;
-          }
-        }
         continue;
       }
       ans.emplace_back(i);
